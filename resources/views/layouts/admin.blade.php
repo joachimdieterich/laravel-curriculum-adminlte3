9fe4aad7f2805ee9b8dfa2da8087552beb771cd6
Joachim Dieterich <mail@joachimdieterich.de>
2019-08-24 09:58:19 +0200

    fixing datatables adding missing jquery

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -96,7 +96,7 @@
         </form>
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
-<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>

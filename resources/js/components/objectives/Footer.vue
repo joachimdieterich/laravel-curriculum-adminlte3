64c9edca3885a5d73d68117ec2f55358c1ae283a
Joachim Dieterich <mail@joachimdieterich.de>
2019-12-16 22:54:33 +0100

    enhance media wallet conditions

--- resources/js/components/objectives/Footer.vue
+++ resources/js/components/objectives/Footer.vue
@@ -1,7 +1,7 @@
 <template >
     <div class="boxfooter row" v-bind:style="{ 'color': textcolor }">
         <div class="p-0 col-12 boxflex">
-            <span v-if="(typeof objective.media_subscriptions[0] !== 'undefined') || (typeof objective.reference_subscriptions[0] !== 'undefined')" >
+            <span v-if="withmedia" >
                 <span class="fa fa-briefcase mr-1"
                     @click.prevent="showModal('objective-medium-modal')">
                 </span>
@@ -39,6 +39,16 @@
         methods: {
             showModal(modal) { 
                 this.$modal.show(modal, {'content': this.objective, 'type': this.type});
+            },
+        },
+        computed: {
+             withmedia: function () {
+                if ((typeof this.objective.media_subscriptions !== 'undefined') || (typeof this.objective.reference_subscriptions !== 'undefined')){
+                     if (this.objective.media_subscriptions.length > 0 || this.objective.reference_subscriptions.length > 0){
+                         return true;
+                     }
+                }
+                return false;
             },
         },
         components: {

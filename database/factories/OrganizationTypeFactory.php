683e0e0fce68cad6da4432f6745686591eb055b2
Joachim Dieterich <mail@joachimdieterich.de>
2019-12-17 09:02:16 +0100

    complete down()

---
+++ database/factories/OrganizationTypeFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\OrganizationType;
+use Faker\Generator as Faker;
+
+$factory->define(OrganizationType::class, function (Faker $faker) {
+    return [
+        'title'       => $faker->company,
+        'external_id' => $faker->numberBetween(1, 100),
+        'state_id'    => 'DE-RP',
+        'country_id'  => 'DE',
+        'created_at'  => null,
+        'updated_at'  => null,
+    ];
+});
+

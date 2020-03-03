7868641ec0b062a3050c3ce599c84970095473ea
Joachim Dieterich <mail@joachimdieterich.de>
2020-03-03 11:43:38 +0100

    adding parent_id

--- app/Http/Controllers/Api/V1/Admin/CurriculaApiController.php
+++ app/Http/Controllers/Api/V1/Admin/CurriculaApiController.php
@@ -48,18 +48,19 @@ class CurriculaApiController extends Controller
         $metadata = array(); //{'id', 'title'}
         
         // generate organizational part of identifier
-        $prefix  = str_pad($curriculum->country()->get()->first()->id, 3, '0', STR_PAD_LEFT).".";
-        $prefix .= str_pad($curriculum->state()->get()->first()->id, 2, '0', STR_PAD_LEFT).".";
-        $prefix .= str_pad($curriculum->organizationType()->get()->first()->external_id, 2, '0', STR_PAD_LEFT).".";
-        $prefix .= str_pad($curriculum->subject()->get()->first()->external_id, 5, '0', STR_PAD_LEFT)."."; //includes subject type!
-        $prefix .= str_pad($curriculum->grade()->get()->first()->external_begin, 2, '0', STR_PAD_LEFT).".";
-        $prefix .= str_pad($curriculum->grade()->get()->first()->external_end, 2, '0', STR_PAD_LEFT).".";
-        $prefix .= str_pad("001", 2, '0', STR_PAD_LEFT).".";
+        $prefix  = str_pad($curriculum->country()->get()->first()->id, 3, '0', STR_PAD_LEFT);
+        $prefix .= str_pad($curriculum->state()->get()->first()->id, 2, '0', STR_PAD_LEFT);
+        $prefix .= str_pad($curriculum->organizationType()->get()->first()->external_id, 2, '0', STR_PAD_LEFT);
+        $prefix .= str_pad($curriculum->subject()->get()->first()->external_id, 5, '0', STR_PAD_LEFT); //includes subject type!
+        $prefix .= str_pad($curriculum->grade()->get()->first()->external_begin, 2, '0', STR_PAD_LEFT);
+        $prefix .= str_pad($curriculum->grade()->get()->first()->external_end, 2, '0', STR_PAD_LEFT);
+        $prefix .= str_pad("001", 2, '0', STR_PAD_LEFT);
         
         // curriculum 
         $metadata[] = [
-            'id'    => $prefix."000.000.000.000", 
-            'title' => $curriculum->title
+            'id'        => $prefix."000000000000", 
+            'title'     => $curriculum->title, 
+            'parent_id' => null
         ];
         
         // foreach terminal_type
@@ -77,20 +78,25 @@ class CurriculaApiController extends Controller
                 
                 $metadata[] = [
                     'id' => $prefix
-                        .str_pad($ter_type, 3, '0', STR_PAD_LEFT)."."
-                        .str_pad($ter, 3, '0', STR_PAD_LEFT)."."
-                        ."000.000", 
-                    'title' => str_replace(array("\n", "\r", "\t"), ' ', strip_tags(ObjectiveType::where('id', $terminalObjective->objective_type_id)->get()->first()->title))
+                        .str_pad($ter_type, 3, '0', STR_PAD_LEFT)
+                        .str_pad($ter, 3, '0', STR_PAD_LEFT)
+                        ."000000", 
+                    'title' => str_replace(array("\n", "\r", "\t"), ' ', strip_tags(ObjectiveType::where('id', $terminalObjective->objective_type_id)->get()->first()->title)),
+                    'parent_id' => $prefix
+                        ."000000000000"
                 ];
                 $ter = 1; //set/reset terminal_id
             } 
             // terminal objective
             $metadata[] = [
                 'id' => $prefix
-                    .str_pad($ter_type, 3, '0', STR_PAD_LEFT)."."
-                    .str_pad($ter, 3, '0', STR_PAD_LEFT)."."
-                    ."000.000", 
-                'title' => str_replace(array("\n", "\r", "\t"), ' ', strip_tags($terminalObjective->title))
+                    .str_pad($ter_type, 3, '0', STR_PAD_LEFT)
+                    .str_pad($ter, 3, '0', STR_PAD_LEFT)
+                    ."000000", 
+                'title' => str_replace(array("\n", "\r", "\t"), ' ', strip_tags($terminalObjective->title)),
+                'parent_id' => $prefix
+                    .str_pad($ter_type, 3, '0', STR_PAD_LEFT)
+                    ."000000000",
             ];
             
             // enabling objective
@@ -99,11 +105,15 @@ class CurriculaApiController extends Controller
             {
                 $metadata[] = [
                 'id' => $prefix
-                    .str_pad($ter_type, 3, '0', STR_PAD_LEFT)."."
-                    .str_pad($ter, 3, '0', STR_PAD_LEFT)."."
-                    .str_pad($ena, 3, '0', STR_PAD_LEFT)."."
+                    .str_pad($ter_type, 3, '0', STR_PAD_LEFT)
+                    .str_pad($ter, 3, '0', STR_PAD_LEFT)
+                    .str_pad($ena, 3, '0', STR_PAD_LEFT)
                     ."000", 
-                'title' => str_replace(array("\n", "\r", "\t"), ' ', (strip_tags($enablingObjective->title)))
+                'title' => str_replace(array("\n", "\r", "\t"), ' ', (strip_tags($enablingObjective->title))),
+                'parent_id' => $prefix
+                    .str_pad($ter_type, 3, '0', STR_PAD_LEFT)
+                    .str_pad($ter, 3, '0', STR_PAD_LEFT)
+                    ."000000", 
             ];
                 $ena++;
             }

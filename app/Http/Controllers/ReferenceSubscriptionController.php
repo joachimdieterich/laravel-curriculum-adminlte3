a0d9e6bed3a0d47af52d5232556f9f51f39d2148
Joachim Dieterich <mail@joachimdieterich.de>
2020-03-17 21:32:02 +0100

    Impement function to generate ReferenceSubscriptions

--- app/Http/Controllers/ReferenceSubscriptionController.php
+++ app/Http/Controllers/ReferenceSubscriptionController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\ReferenceSubscription;
+use App\Reference;
+use App\Curriculum;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ReferenceSubscriptionController extends Controller
 {
@@ -35,7 +38,44 @@ class ReferenceSubscriptionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $new_subscription   = $this->validateRequest();
+        $uuid               = Str::uuid();
+        if (($new_subscription['subscribable_type'] ==  "App\TerminalObjective" AND $new_subscription['terminal_objective_id'] == $new_subscription['subscribable_id'])
+            OR
+            ($new_subscription['subscribable_type'] ==  "App\EnablingObjective" AND $new_subscription['enabling_objective_id'] == $new_subscription['subscribable_id'])
+           )
+        {
+            return false;
+        }
+        
+        $reference = Reference::Create([
+            'id'          => (string) Str::uuid(),
+            'description' => '',
+            'grade_id'    => isset($new_subscription['grade_id']) ? $new_subscription['grade_id'] : Curriculum::find($new_subscription['curriculum_id'])->grade_id,
+            'owner_id'    => auth()->user()->id,	
+        ]);
+        $subscription = ReferenceSubscription::Create([
+            'reference_id'       => $reference->id,
+            'referenceable_type' => $new_subscription['subscribable_type'],
+            'referenceable_id'   => $new_subscription['subscribable_id'],
+            'sharing_level_id'   => isset($new_subscription['sharing_level_id']) ? $new_subscription['sharing_level_id'] : 1,
+            'visibility'         => isset($new_subscription['visibility']) ? $new_subscription['visibility'] : true,
+            'owner_id'           => auth()->user()->id,	
+        ]);
+        
+        if ($subscription){ //generate sibling
+            $subscription = ReferenceSubscription::Create([
+                'reference_id'       => $reference->id,
+                'referenceable_type' => ($new_subscription['enabling_objective_id'] != null) ? "App\EnablingObjective" : "App\TerminalObjective",
+                'referenceable_id'   => ($new_subscription['enabling_objective_id'] != null)  ? $new_subscription['enabling_objective_id'] : $new_subscription['terminal_objective_id'],
+                'sharing_level_id'   => isset($new_subscription['sharing_level_id']) ? $new_subscription['sharing_level_id'] : 1,
+                'visibility'         => isset($new_subscription['visibility']) ? $new_subscription['visibility'] : true,
+                'owner_id'           => auth()->user()->id,	
+            ]);
+        }
+        if (request()->wantsJson()){    
+            return ['message' => 'ok'];
+        }
     }
 
     /**
@@ -107,5 +147,17 @@ class ReferenceSubscriptionController extends Controller
                                 ->where('referenceable_type', '=', $referenceSubscription->referenceable_type);
                             }])->get();
 
+    }
+    
+    protected function validateRequest()
+    {   
+        return request()->validate([
+            "curriculum_id" => 'sometimes|required',
+            "grade_id" => 'sometimes',
+            "terminal_objective_id" => 'sometimes|required',
+            "enabling_objective_id" => 'sometimes',
+            "subscribable_type" => 'required',
+            "subscribable_id" => 'required',
+        ]);
     }
 }

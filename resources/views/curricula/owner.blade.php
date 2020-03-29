1c7dc769516824af01828f68597fc74e398f7b8d
Joachim Dieterich <mail@joachimdieterich.de>
2020-03-29 11:05:30 +0200

    Change owner of curriculum #55

---
+++ resources/views/curricula/owner.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+@section('title')
+     {{ trans('global.curriculum.edit_owner') }}
+@endsection
+@section('breadcrumb')
+    <li class="breadcrumb-item "><a href="/">{{ trans('global.home') }}</a></li>
+    <li class="breadcrumb-item active">{{ trans('global.curriculum.edit_owner') }}</li>
+    <li class="breadcrumb-item "><a href="/documentation" class="text-black-50"><i class="fas fa-question-circle"></i></a></li>
+@endsection
+@section('content')
+
+<div class="card">
+    <div class="card-body">
+        <form action="{{ route('curricula.storeOwner', $curriculum->id) }}" 
+              method="POST" 
+              enctype="multipart/form-data">
+            @method('PATCH')
+            @csrf
+           
+            @include ('forms.input.select', 
+                    ["model" => "user",
+                    "show_label" => true,
+                    "class_left" => "col-12 px-0",
+                    "placeholder" => trans('global.owner') ,
+                    "field" => "owner_id",  
+                    "options"=> $users, 
+                    "option_id" => "id",
+                    "option_label"=> "username", 
+                    "value" => old('owner_id', isset($curriculum->owner_id) ? $curriculum->owner_id : '') ]) 
+            <div>
+                <input class="btn btn-info" type="submit" value="{{ trans('global.save') }}">
+            </div>
+        </form>
+    </div>
+</div>
+@endsection

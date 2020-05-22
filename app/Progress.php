e6f372381ac78af0c7773ce8bc96005f885b6605
Joachim Dieterich <mail@joachimdieterich.de>
2020-05-22 18:56:12 +0200

    possible plural bug fix

--- app/Progress.php
+++ app/Progress.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Progress extends Model
 {
     protected $guarded = [''];
+    protected $table = 'progresses';
+   
     public function referenceable()
     {
         return $this->morphTo();

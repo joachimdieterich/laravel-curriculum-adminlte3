5c9cb40a061a7c764e49a5cbe5676e47911f4571
Joachim Dieterich <mail@joachimdieterich.de>
2020-06-01 20:32:55 +0200

    adding unique identifier fields to curricula, terminal and enabling objectives

---
+++ database/migrations/2020_06_01_202615_add_uid_to_terminal_objectives.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUidToTerminalObjectives extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('terminal_objectives', function (Blueprint $table) {
+            $table->char('ui', 31);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('terminal_objectives', function (Blueprint $table) {
+            $table->dropColumn('ui');//
+        });
+    }
+}

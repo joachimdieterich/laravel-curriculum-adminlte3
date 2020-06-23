408f0c610a7bc5e570889ef363de80b42961012e
Joachim Dieterich <mail@joachimdieterich.de>
2020-06-23 22:31:14 +0200

    adding eventSubscription class

---
+++ database/migrations/2020_06_21_220511_create_event_subscriptions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('subscribable_type');
+            $table->unsignedbigInteger('subscribable_id');
+            $table->string('plugin_name'); //eg 'evewa'
+            $table->string('value',500); 
+            $table->unsignedbigInteger('sharing_level_id');
+            $table->boolean('visibility');
+            $table->unsignedbigInteger('owner_id');
+            $table->timestamps();
+
+            $table->foreign('sharing_level_id')->references('id')->on('sharing_levels');
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_subscriptions');
+    }
+}

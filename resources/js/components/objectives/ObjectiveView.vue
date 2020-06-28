7395fdb1b99bf48db1a7da7667248922b3167419
Joachim Dieterich <mail@joachimdieterich.de>
2020-06-28 14:03:46 +0200

    Close #78: Hide description tab on ObjectiveView if description is empty

--- resources/js/components/objectives/ObjectiveView.vue
+++ resources/js/components/objectives/ObjectiveView.vue
@@ -17,7 +17,7 @@
                                         </a>
                                 </li>
                                 
-                                <li class="nav-item">
+                                <li v-if="objective.description != ''" class="nav-item">
                                     <a class="nav-link " 
                                        href="#description" 
                                        data-toggle="tab">{{ trans('global.description') }}</a>
@@ -318,7 +318,7 @@
              },
              loadExternal: function() {
                     this.$refs.repositoryPlugin.loader();
-                    this.$refs.eventPlugin.loader();
+                    //this.$refs.eventPlugin.loader();
              }
             
         },

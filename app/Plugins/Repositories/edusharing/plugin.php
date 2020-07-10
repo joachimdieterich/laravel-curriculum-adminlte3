24e85b563f3aa35693c7b993d83ddcb5731a3530
Joachim Dieterich <mail@joachimdieterich.de>
2020-07-10 09:10:14 +0200

    debug edusharing plugin

--- app/Plugins/Repositories/edusharing/plugin.php
+++ app/Plugins/Repositories/edusharing/plugin.php
@@ -318,7 +318,7 @@ class Edusharing extends RepositoryPlugin
 
     //https://[EDUSHARINGDOMAIN]/edu-sharing/rest/search/v1/custom/-home-?contentType=ALL&property=cm:name&value=curriculum&maxItems=10&skipCount=0
     public function getSearchCustom($repository, $params) {
-        dump($this->repoUrl . '/rest/search/v1/custom/' . $repository.'?'.http_build_query($params));
+        //dump($this->repoUrl . '/rest/search/v1/custom/' . $repository.'?'.http_build_query($params));
         $ret =$this->call ( $this->repoUrl . '/rest/search/v1/custom/' . $repository.'?'.http_build_query($params));
         return json_decode($ret, true);
     }

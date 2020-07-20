5002554379aa0fd281fd8eb1ceaf982f89c021a5
Joachim Dieterich <mail@joachimdieterich.de>
2020-07-20 13:21:17 +0200

    removing tinymce plugins, now in core available

--- resources/views/forms/input/textarea.blade.php
+++ resources/views/forms/input/textarea.blade.php
@@ -42,8 +42,8 @@
     plugins: [
       "advlist autolink lists link image charmap print preview hr anchor pagebreak",
       "searchreplace wordcount visualblocks visualchars code fullscreen",
-      "insertdatetime media nonbreaking save table contextmenu directionality",
-      "emoticons template paste textcolor colorpicker textpattern"
+      "insertdatetime media nonbreaking save table directionality",
+      "emoticons template paste textpattern"
     ],
     toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
     relative_urls: false,

4ef43ce0c42b49186bfd3d7cbf798181cc771007
Joachim Dieterich <mail@joachimdieterich.de>
2020-07-21 15:39:07 +0200

    wip

---
+++ database/migrations/2020_07_15_134335_create_plan_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plan_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plan_types');
+    }
+}
---
+++ database/migrations/2020_07_19_134121_create_plans_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamp('begin');
+            $table->timestamp('end')->nullable();
+            $table->integer('duration')->nullable();
+            
+            $table->unsignedbigInteger('type_id')->nullable();
+            $table->unsignedbigInteger('owner_id')->nullable();
+            
+            $table->timestamps();
+            
+            $table->foreign('type_id')->references('id')->on('plan_types');
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}

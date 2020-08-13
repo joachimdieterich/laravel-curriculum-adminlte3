a3482870b33eb908f3e15ca624e10f59c929e82c
Joachim Dieterich <mail@joachimdieterich.de>
2020-08-13 19:23:57 +0200

    Optimize datatable-queries (N+1) and gate Issue #95

--- app/Http/Controllers/PermissionsController.php
+++ app/Http/Controllers/PermissionsController.php
@@ -23,19 +23,22 @@ class PermissionsController extends Controller
         abort_unless(\Gate::allows('permission_access'), 403);
 
         $permissions = Permission::all();
+         
+        $edit_gate = \Gate::allows('user_edit');
+        $delete_gate = \Gate::allows('user_delete');
         
         return DataTables::of($permissions)
           
-            ->addColumn('action', function ($permissions) {
+            ->addColumn('action', function ($permissions) use ($edit_gate, $delete_gate) {
                  $actions  = '';
-                    if (\Gate::allows('permission_edit')){
+                    if ($edit_gate){
                         $actions .= '<a href="'.route('permissions.edit', $permissions->id).'" '
                                     . 'id="permission-user-'.$permissions->id.'" '
                                     . 'class="btn">'
                                     . '<i class="fa fa-pencil-alt"></i>'
                                     . '</a>';
                     }
-                    if (\Gate::allows('permission_delete')){
+                    if ($delete_gate){
                         $actions .= '<button type="button" '
                                 . 'class="btn text-danger" '
                                 . 'onclick="destroyDataTableEntry(\'permissions\','.$permissions->id.')">'

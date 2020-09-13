3eb519e7a025fa443cc9575df602eccc934dc9f1
Joachim Dieterich <mail@joachimdieterich.de>
2020-09-13 19:50:35 +0200

    adding optgroup_icon/optgroup_class and option_icon/option_class to enable dynamic styling

--- resources/views/forms/input/select.blade.php
+++ resources/views/forms/input/select.blade.php
@@ -1,60 +1,73 @@
-<div id="{{ $field }}_form_group" 
-     class="form-group {{ $errors->has( $field ) ? ' has-danger' : '' }} {{ isset($css) ? $css : '' }}" 
+<div id="{{ $field }}_form_group"
+     class="form-group {{ $errors->has( $field ) ? ' has-danger' : '' }} {{ isset($css) ? $css : '' }}"
 >
-    @if(isset($show_label))      
+    @if(isset($show_label))
         <label for="{{ $field }}" class="{{ isset($class_left) ? $class_left : 'p-0 col-sm-12' }}">
-            
+
             {{ isset($label) ? $label :trans("global.{$model}.title_singular") }}
-          
-            @if(isset($multiple)) 
+
+            @if(isset($multiple))
             <span class="btn btn-info btn-xs deselect-all pull-right">{{trans("global.deselect_all")}}</span>
             <span class="btn btn-info btn-xs select-all pull-right mr-1">{{trans("global.select_all")}}</span>
-            
+
             @endif
         </label>
-    
+
     @endif
-        <select name="{{ $field }}[]" 
-                id="{{ $field }}" 
+        <select name="{{ $field }}[]"
+                id="{{ $field }}"
                 class="form-control select2 {{ $errors->has($field) ? ' is-invalid' : '' }} {{ isset($class_right) ? $class_right : 'col-sm-12' }}"
                 style="{{ isset($style) ? $style : 'width:100%;' }}"
-                @if(isset($onchange)) 
+                @if(isset($onchange))
                     onchange="{{ $onchange }}"
                 @endif
-                onchange="form-control select2{{ $errors->has($field) ? ' is-invalid' : '' }}" 
-                @if(isset($multiple)) 
+                @if(isset($multiple))
                     multiple="multiple"
                 @endif
                >
 
             <?php $current_optgroup_id = 'false'; ?>
-            @if(!isset($multiple)) 
+            @if(!isset($multiple))
             <option></option>
             @endif
             @foreach($options as $v)
-                <?php $o_id = isset($option_id) ? $option_id : 'id'; ?>  
-                    
+                <?php $o_id = isset($option_id) ? $option_id : 'id'; ?>
+
                     @if (isset($optgroup[0]) && ($current_optgroup_id != $v->$optgroup_reference_field ))
                          <?php $optgroup_label = ((isset($optgroup_label)) ? $optgroup_label : 'title');
-                             $opt_label = $optgroup->where((isset($optgroup_id)) ? $optgroup_id : 'id', $v->$optgroup_reference_field)->first()->$optgroup_label 
+                             $opt_label = $optgroup->where((isset($optgroup_id)) ? $optgroup_id : 'id', $v->$optgroup_reference_field)->first()->$optgroup_label
                                      ?>
-                        <optgroup id="{{ $v->$optgroup_reference_field }}" label="{{ $opt_label }}">
+                        <optgroup
+                            id="{{ $v->$optgroup_reference_field }}"
+                            label="{{ $opt_label }}"
+                            @if (isset($optgroup_class))
+                                data-class="{{ $optgroup_class }}"
+                            @endif
+                            @if (isset($optgroup_icon))
+                                data-icon="{{ $optgroup_icon }}"
+                            @endif
+                        >
                     @endif
-                    
-                        <option value="{{ $v->$o_id }}" {{ ( $v->$o_id == $value ) ? 'selected' : '' }}>
+
+                        <option
+                            value="{{ $v->$o_id }}" {{ ( $v->$o_id == $value ) ? 'selected' : '' }}
+                            @if (isset($option_icon))
+                                data-icon="{{ $option_icon }}"
+                            @endif
+                        >
                            {{ (isset($option_label)) ? $v->$option_label :$v->title }}
                             @if (isset($combine_labels) ? $combine_labels : false)
                                 | {{ $opt_label }}
                             @endif
                         </option>
-                
+
                     @if (isset($optgroup[0]))
                         @if ($current_optgroup_id != $v->$optgroup_reference_field )
                          </optgroup>
                         @endif
                         <?php $current_optgroup_id = $v->$optgroup_reference_field ?>
                     @endif
-                
+
             @endforeach
         </select>
         @if ($errors->has( $field ))
@@ -67,13 +80,18 @@
     <!--hack to get select2 working-->
     <script>
     $(document).ready(function() {
+        function formatText (icon) {
+            return $('<span class="' + $(icon.element).data('class') + '"><i class="fas ' + $(icon.element).data('icon') + '"></i> ' + icon.text + '</span>');
+        }
         <!--hack to get select2 working z-index-->
         $("#{{ $field }}").select2({
             placeholder: "{{ $placeholder }}",
             dropdownParent: $("#{{ $field }}").parent(),
-            allowClear: "{{ $allowClear ?? true }}" 
+            allowClear: "{{ $allowClear ?? true }}",
+            templateSelection: formatText,
+            templateResult: formatText
         });
     });
     </script>
     @endsection
- @endif
+@endif

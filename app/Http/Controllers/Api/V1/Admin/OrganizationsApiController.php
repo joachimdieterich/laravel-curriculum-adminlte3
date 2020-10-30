f22dc078fbdb0e2c988c2d5c18e9aa1e2b71f0ec
Joachim Dieterich <mail@joachimdieterich.de>
2020-10-30 11:06:22 +0100

    fix org expel

--- app/Http/Controllers/Api/V1/Admin/OrganizationsApiController.php
+++ app/Http/Controllers/Api/V1/Admin/OrganizationsApiController.php
@@ -5,43 +5,44 @@ namespace App\Http\Controllers\Api\V1\Admin;
 use App\Http\Controllers\Controller;
 use App\Organization;
 use App\OrganizationRoleUser;
+use App\User;
 
 class OrganizationsApiController extends Controller {
 
-    public function index() 
+    public function index()
     {
         $organizations = Organization::all();
 
         return $organizations;
     }
 
-    public function store() 
+    public function store()
     {
         return Organization::create($this->filteredRequest());
     }
 
-    public function update(Organization $organization) 
+    public function update(Organization $organization)
     {
-        if ($organization->update($this->filteredRequest())) 
+        if ($organization->update($this->filteredRequest()))
         {
             return $organization->fresh();
         }
     }
 
-    public function show(Organization $organization) 
+    public function show(Organization $organization)
     {
         return $organization;
     }
 
-    public function destroy(Organization $organization) 
+    public function destroy(Organization $organization)
     {
         if ($organization->delete()) {
             return ['message' => 'Successful deleted'];
         }
     }
 
-    public function enrol() 
-    {   
+    public function enrol()
+    {
         return OrganizationRoleUser::updateOrCreate(
                 [
                         'user_id'         => request('user_id'),
@@ -53,14 +54,21 @@ class OrganizationsApiController extends Controller {
                 );
     }
 
-    public function expel() 
+    public function expel()
     {
         if (OrganizationRoleUser::where(request()->all())->delete())
         {
+            //reset current org id
+            $user = User::where('id', request('user_id'));
+            $user->current_organization_id = NULL;
+            $user->current_period_id = NULL;
+            $user->save();
+
             return ['message' => 'Successful expelled'];
+
         }
     }
-    
+
     public function members(Organization $organization) {
         return $organization->users;
     }

d12f8a047051e0446951eaa607720765cf37a656
Joachim Dieterich <mail@joachimdieterich.de>
2020-11-26 08:11:18 +0100

    psr-4 autoloading standard

--- tests/Unit/PeriodTest.php
+++ tests/Unit/PeriodTest.php
@@ -6,25 +6,25 @@ use Tests\TestCase;
 use App\Period;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class PeriodtTest extends TestCase
+class PeriodTest extends TestCase
 {
     use RefreshDatabase;
-     
+
     /** @test */
-    public function it_has_an_owner() 
+    public function it_has_an_owner()
     {
         $this->signInAdmin();
-        $period = Period::findOrFail(1); 
-        
+        $period = Period::findOrFail(1);
+
         $this->assertInstanceOf('App\USER', $period->owner()->first());
     }
-    
+
     /** @test */
-    public function it_has_a_period() 
+    public function it_has_a_period()
     {
         $this->signInAdmin();
-        $period = Period::findOrFail(1); 
-        
+        $period = Period::findOrFail(1);
+
         $this->assertInstanceOf('App\Organization', $period->organization()->first());
 
     }

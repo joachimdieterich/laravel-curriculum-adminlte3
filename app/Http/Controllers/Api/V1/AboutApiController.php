4ab487ba3b3590b1993cfd5c9c54d52ff3b8b9e1
Joachim Dieterich <mail@joachimdieterich.de>
2020-11-26 07:56:19 +0100

    psr-4 autoloading standard

--- app/Http/Controllers/Api/V1/AboutApiController.php
+++ app/Http/Controllers/Api/V1/AboutApiController.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\API\V1;
+namespace App\Http\Controllers\Api\V1;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\User;
 
 
 
 class AboutApiController extends Controller
 {
-    
+
     public function index()
     {
         return "Curriculum API V1 (about) works";

b816433974dc1b5d3e3d48742c74dddcefe42a73
Joachim Dieterich <mail@joachimdieterich.de>
2020-11-27 19:32:15 +0100

    psr-4 autoloading standard

--- tests/Feature/AchievementCRUDTest.php
+++ tests/Feature/AchievementCRUDTest.php
@@ -4,13 +4,12 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Facades\Tests\Setup\AchievementFactory;
 use Illuminate\Support\Facades\Auth;
 
 class AchievementCRUDTest extends TestCase
 {
     use RefreshDatabase;
-     
+
     public function setUp(): void
     {
         parent::setUp();
@@ -20,35 +19,35 @@ class AchievementCRUDTest extends TestCase
         $this->post("terminalObjectives" , factory('App\TerminalObjective')->raw());
         $this->post("enablingObjectives" , factory('App\EnablingObjective')->raw());
     }
-  
-    /** @test 
+
+    /** @test
      * Use Route: POST, achievements, achievements.store
-     */     
+     */
     public function an_user_can_set_the_achievement_status_for_an_enabling_objective()
-    { 
+    {
         $attributes = factory('App\Achievement')->raw();
         $attributes['owner_id'] = auth()->user()->id;
         $attributes['user_id'] = auth()->user()->id;
-        
+
         $this->post("achievements" , $attributes);
         $attributes['status'] = "10";
         $this->assertDatabaseHas('achievements', $attributes);
     }
-    
-    /** @test 
+
+    /** @test
      * Use Route: POST, achievements, achievements.store
-     */     
+     */
     public function an_admin_can_set_the_achievement_status_for_an_enabling_objective()
-    { 
+    {
         Auth::logout(); //logout student
         $this->signInAdmin();
         $attributes = factory('App\Achievement')->raw();
         $attributes['owner_id'] = auth()->user()->id;
         $attributes['user_id'] = 2; //set achievement for studend (id 2)
-        
+
         $this->post("achievements" , $attributes);
         $attributes['status'] = "01";
         $this->assertDatabaseHas('achievements', $attributes);
     }
-   
+
 }

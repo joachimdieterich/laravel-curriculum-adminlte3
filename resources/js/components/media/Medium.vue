76f12a9ded6778fa87e5a3beebd184a28dcce23f
Joachim Dieterich <mail@joachimdieterich.de>
2020-12-02 17:13:15 +0100

    enhance images

--- resources/js/components/media/Medium.vue
+++ resources/js/components/media/Medium.vue
@@ -1,13 +1,13 @@
 <template >
 
     <div
-        v-bind:id="'medium_'+medium.id" 
-        class="box box-objective pointer my-1" 
-        style="height: 300px !important; min-width: 200px !important; padding: 0; background-size: cover;"
+        v-bind:id="'medium_'+medium.id"
+        class="box box-objective pointer my-1"
+        style="height: 300px !important; min-width: 200px !important; padding: 0; background-size: 100%,50%;"
         :style="{'background-image':'url('+href+')'}"
         @click="show('medium', medium)"
-    >    
-        <div class="symbol" 
+    >
+        <div class="symbol"
              style="position: absolute;
                 padding: 6px;
                 z-index: 1;
@@ -17,35 +17,35 @@
                 top: 0px;
                 font-size: 1.2em;
                 left: 10px;">
-            
+
             <i v-if="medium.mime_type === 'application/pdf'" class="fa fa-file-pdf text-white pt-2"></i>
             <i v-if="medium.mime_type === 'url'" class="fa fa-link text-white pt-2"></i>
             <i v-else class="fa fa-photo-video text-white pt-2"></i>
         </div>
-        
-        <i v-if="medium.mime_type === 'application/pdf'" class="far fa-file-pdf text-primary text-center pt-2" 
+
+        <i v-if="medium.mime_type === 'application/pdf'" class="far fa-file-pdf text-primary text-center pt-2"
            style="position:absolute; top: 0px; height: 150px !important; width: 100%; font-size:800%;"></i>
-        <i v-if="medium.mime_type === 'url'" class="fa fa-link text-primary text-center pt-2" 
+        <i v-if="medium.mime_type === 'url'" class="fa fa-link text-primary text-center pt-2"
            style="position:absolute; top: 0px; height: 150px !important; width: 100%; font-size:800%;"></i>
-        <span 
+        <span
             v-can="'medium_delete'"
-            class="p-1 pointer_hand" 
-            accesskey="" 
+            class="p-1 pointer_hand"
+            accesskey=""
             style="position:absolute; top:0px; height: 30px; width:100%;"
             >
-                <button 
+                <button
                     :id="'delete-medium'+medium.id"
-                    type="submit" 
+                    type="submit"
                     class="btn btn-danger btn-sm pull-right"
                     v-on:click.stop="unlinkMedium();"
                     >
                     <small>
-                        <i class="fa fa-unlink"  
+                        <i class="fa fa-unlink"
                            ></i>
                     </small>
                 </button>
-        </span>  
-        <span class="bg-white text-center p-1 overflow-auto " 
+        </span>
+        <span class="bg-white text-center p-1 overflow-auto "
             style="position:absolute; bottom:0px; height: 150px; width:100%;">
             <h6 class="events-heading pt-1 hyphens" v-html="medium.title"></h6>
             <p class=" text-muted small" v-html="medium.description"></p>
@@ -56,7 +56,7 @@
 
 
 <script>
-    
+
     export default {
         props: {
                 subscription: {},
@@ -68,7 +68,7 @@
             }
         },
         methods: {
-           show(model, entry) {   
+           show(model, entry) {
                 this.$modal.show(model.toLowerCase()+'-modal', { 'content': entry});
             },
             async unlinkMedium() { //id of external reference and value in db
@@ -78,7 +78,7 @@
                     //this.errors = error.response.data.errors;
                 }
                 $("#medium_"+this.medium.id).hide();
-            },   
+            },
         },
         computed: {
             href: function () {
@@ -92,6 +92,6 @@
                 this.errors = error.response.data.errors;
             });
         },
-   
+
     }
 </script>

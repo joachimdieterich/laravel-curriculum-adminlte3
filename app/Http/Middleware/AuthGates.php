ab4f0c352c6e9c91c6c8c566557c0bd39a51dba7
Joachim Dieterich <mail@joachimdieterich.de>
2020-12-09 19:18:09 +0100

    close ##118: Gate should check based only on current role.

--- app/Http/Middleware/AuthGates.php
+++ app/Http/Middleware/AuthGates.php
@@ -12,9 +12,9 @@ class AuthGates
 {
     public function handle($request, Closure $next)
     {
-        
+
         $user = \Auth::user();
-       
+
         if (/* !app()->runningInConsole() && */ $user) { // get permissions in console
             //$roles = Role::with('permissions')->get();
             $roles =  Cache::rememberForever('roles', function () {
@@ -28,11 +28,12 @@ class AuthGates
             }
 
             foreach ($permissionsArray as $title => $roles) {
-                Gate::define($title, function (\App\User $user) use ($roles) {
-                    return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0;
+                Gate::define($title, function (\App\User $user) use ($title, $roles) {
+                    return in_array($user->role()->id, $roles) ? true : false; //only check current role
+                    //return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0; //check all users roles
                 });
             }
-            
+
             //set current organization and current period if not set
             if ($user->current_organization_id === NULL)
             {
@@ -45,14 +46,14 @@ class AuthGates
                 $user->current_period_id = optional(DB::table('periods')
                         ->select('periods.*')
                         ->join('groups', 'groups.period_id', '=', 'periods.id')
-                        ->join('group_user', 'group_user.group_id', '=', 'groups.id') 
+                        ->join('group_user', 'group_user.group_id', '=', 'groups.id')
                         ->where('group_user.user_id',  $user->id)
                         ->where('groups.organization_id', $user->current_organization_id)
                         ->get()->first())->id;
                 $user->save();
-            }  
-            
-        } 
+            }
+
+        }
 
         return $next($request);
     }

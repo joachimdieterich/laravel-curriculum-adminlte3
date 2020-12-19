534574c6defe0afbdc9439437af97ca4f2395ac7
Joachim Dieterich <mail@joachimdieterich.de>
2020-12-19 12:07:17 +0100

    enhance scrollbars (windows)

--- resources/views/partials/features/box.blade.php
+++ resources/views/partials/features/box.blade.php
@@ -1,5 +1,5 @@
 <div id="{{ $id }}"
-                    class="box box-objective pointer card" 
+                    class="box box-objective pointer card"
                     style="height: 300px !important; min-width: 200px !important; padding: 0; background-color: {{ $color }}; background-size: cover;">
                     <span  style="position:absolute" class="w-100 h-100 center-icon p-3">
                         <i class="{{ $icon }}"></i>
@@ -7,10 +7,10 @@
                           {{ $title }}
                         </h6>
                     </span>
-                    <span class="bg-white p-1 overflow-auto card-body" 
+                    <span class="bg-white p-1 hide-scrollbars card-body"
                          style="position:absolute; bottom:0px; height: 150px; width:100%;">
                         {!! $content !!}
-                           
+
 <!--                       <p class="text-primary small p-2 mb-0"
                            style="position:absolute; bottom:0px; right:0px">
                          mehr...

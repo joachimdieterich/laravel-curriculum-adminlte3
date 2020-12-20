aba948ae1f4966412112477c00a417e7bcbc0179
Joachim Dieterich <mail@joachimdieterich.de>
2020-12-20 22:45:44 +0100

    close #122: New Organisation-Type: Error 500 if you click on new organisation-types / federal-state is not saved correctly

--- resources/views/organizationtypes/form.blade.php
+++ resources/views/organizationtypes/form.blade.php
@@ -1,34 +1,28 @@
 @csrf
-@include ('forms.input.text', 
-            ["model" => "organizationtype", 
-            "field" => "title", 
-            "placeholder" => "curriculum Headquarter",  
-            "required" => true, 
-            "value" => old('title', isset($organizationtype) ? $organizationtype->title : '')])
+@include ('forms.input.text',
+            ["model" => "organizationtype",
+            "field" => "title",
+            "placeholder" => "curriculum Headquarter",
+            "required" => true,
+            "value" => old('title', isset($organizationtype->title) ? $organizationtype->title : '')])
 
-@include ('forms.input.text', 
-            ["model" => "organizationtype", 
-            "field" => "external_id", 
-            "value" => old('street', isset($organizationtype) ? $organizationtype->external_id : '')])
+@include ('forms.input.text',
+            ["model" => "organizationtype",
+            "field" => "external_id",
+            "value" => old('street', isset($organizationtype->external_id) ? $organizationtype->external_id : 0)])
 
-@include ('forms.input.select', 
-            ["model" => "state", 
-            "show_label" => true,
-            "field" => "state_id",  
-            "options"=> $states, 
-            "option_label" => "lang_de",    
-            "option_id" => "code",    
-            "optgroup" => $countries,    
-            "optgroup_label" => "lang_de",
-            "optgroup_id" => "alpha2",
-            "optgroup_reference_field" => "country",    
-            "value" =>  old('state_id', isset($organizationtype->state_id) ? $organizationtype->state_id : '')])                                                    
+@include ('forms.input.country', [
+            "countries" => $countries,
+            "country_id" => old('street', isset($organizationtype->country_id) ? $organizationtype->country_id : 'DE'),
+            "states" => $states,
+            "state_id" => old('street', isset($organizationtype->state_id) ? $organizationtype->state_id : 'DE-RP')
+         ])
 
 <div>
-    <input 
+    <input
         id="organizationtype-save"
-        class="btn btn-info" 
-        type="submit" 
+        class="btn btn-info"
+        type="submit"
         value="{{ $buttonText }}"
     >
 </div>

0ac963c38c6ec9c03d03130e4b98bb0be3437ef5
Joachim Dieterich <mail@joachimdieterich.de>
2020-12-21 10:55:13 +0100

    close #123: Guest-Role: Error 500 when clicking "Logbücher" Guest should have only the followingn roles: 'curriculum_show', 'navigator_show',

--- database/seeds/PermissionRoleTableSeeder.php
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -10,31 +10,31 @@ class PermissionRoleTableSeeder extends Seeder
     {   //set admin permissions
         $admin_permissions = Permission::all();
         Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
-        
+
         //set creator permissions
         $creator_permissions = $admin_permissions->filter(function ($permission) {
             $creator_permission_list = [
-                'categorie_access', 
-                'categorie_create', 
-                'categorie_show', 
-                'categorie_edit', 
-                'categorie_delete', 
-                'certificate_access', 
-                'certificate_create', 
-                'certificate_show', 
-                'certificate_edit', 
-                'certificate_delete', 
-                'content_access', 
-                'content_create', 
+                'categorie_access',
+                'categorie_create',
+                'categorie_show',
+                'categorie_edit',
+                'categorie_delete',
+                'certificate_access',
+                'certificate_create',
+                'certificate_show',
+                'certificate_edit',
+                'certificate_delete',
+                'content_access',
+                'content_create',
                 'content_show',
                 'content_edit',
                 'content_delete',
-                'course_create', 
-                'course_show', 
-                'course_edit', 
-                'course_delete', 
-                'curriculum_access', 
-                'curriculum_create', 
+                'course_create',
+                'course_show',
+                'course_edit',
+                'course_delete',
+                'curriculum_access',
+                'curriculum_create',
                 'curriculum_show',
                 'curriculum_edit',
                 'curriculum_delete',
@@ -53,11 +53,11 @@ class PermissionRoleTableSeeder extends Seeder
                 'logbook_entry_show',
                 'logbook_entry_edit',
                 'logbook_entry_delete',
-                'navigator_access', 
-                'navigator_create', 
-                'navigator_show', 
-                'navigator_edit', 
-                'navigator_delete', 
+                'navigator_access',
+                'navigator_create',
+                'navigator_show',
+                'navigator_edit',
+                'navigator_delete',
                 'objective_create',
                 'objective_edit',
                 'objective_delete',
@@ -77,16 +77,16 @@ class PermissionRoleTableSeeder extends Seeder
                 'task_edit',
                 'task_delete',
             ];
-            
+
             return in_array($permission->title, $creator_permission_list);
         });
         Role::findOrFail(2)->permissions()->sync($creator_permissions);
-        
+
         //set Indexer permissions
         $indexer_permissions = $admin_permissions->filter(function ($permission) {
             $indexer_permission_list = [
                 'curriculum_show',
-                'navigator_show', 
+                'navigator_show',
                 'logbook_access',
                 'logbook_show',
                 'logbook_entry_access',
@@ -94,24 +94,24 @@ class PermissionRoleTableSeeder extends Seeder
                 'task_access',
                 'task_show',
             ];
-            
+
             return in_array($permission->title, $indexer_permission_list);
         });
         Role::findOrFail(3)->permissions()->sync($indexer_permissions);
-        
+
         //set schooladmin permissions
         $schooladmin_permissions = $admin_permissions->filter(function ($permission) {
             $schooladmin_permission_list = [
-                'achievement_access', 
-                'achievement_create', 
-                'categorie_access',  
-                'categorie_show', 
-                'certificate_access', 
-                'certificate_show', 
-                'course_create', 
-                'course_show', 
-                'course_edit', 
-                'course_delete', 
+                'achievement_access',
+                'achievement_create',
+                'categorie_access',
+                'categorie_show',
+                'certificate_access',
+                'certificate_show',
+                'course_create',
+                'course_show',
+                'course_edit',
+                'course_delete',
                 'curriculum_show',
                 'group_access',
                 'group_create',
@@ -144,30 +144,30 @@ class PermissionRoleTableSeeder extends Seeder
                 'task_edit',
                 'task_delete',
             ];
-            
+
             return in_array($permission->title, $schooladmin_permission_list);
         });
         Role::findOrFail(4)->permissions()->sync($schooladmin_permissions);
-        
+
         //set teacher permissions
         $teacher_permissions = $admin_permissions->filter(function ($permission) {
             $teacher_permission_list = [
-                'achievement_access', 
-                'achievement_create', 
-                'categorie_access', 
-                'categorie_show', 
-                'certificate_access', 
-                'certificate_show', 
-                'content_access', 
-                'content_create', 
+                'achievement_access',
+                'achievement_create',
+                'categorie_access',
+                'categorie_show',
+                'certificate_access',
+                'certificate_show',
+                'content_access',
+                'content_create',
                 'content_show',
                 'content_edit',
                 'content_delete',
-                'course_create', 
-                'course_show', 
-                'course_edit', 
-                'course_delete', 
-                'curriculum_show', 
+                'course_create',
+                'course_show',
+                'course_edit',
+                'course_delete',
+                'curriculum_show',
                 'group_access',
                 'group_show',
                 'logbook_access',
@@ -191,34 +191,34 @@ class PermissionRoleTableSeeder extends Seeder
                 'task_edit',
                 'task_delete',
             ];
-            
+
             return in_array($permission->title, $teacher_permission_list);
         });
         Role::findOrFail(5)->permissions()->sync($teacher_permissions);
-        
+
         //set students permissions
         $user_permissions = $admin_permissions->filter(function ($permission) {
             $user_permission_list = [
-                'achievement_create', 
+                'achievement_create',
                 'curriculum_show',
-                'content_access', 
-                'content_create', 
+                'content_access',
+                'content_create',
                 'content_show',
                 'logbook_access',
                 'logbook_show',
                 'logbook_entry_access',
                 'logbook_entry_show',
-                'navigator_access', 
-                'navigator_show', 
+                'navigator_access',
+                'navigator_show',
                 'task_access',
                 'task_show',
-                
+
             ];
-            
+
             return in_array($permission->title, $user_permission_list);
         });
         Role::findOrFail(6)->permissions()->sync($user_permissions);
-        
+
         //set parents permissions
         $parent_permissions = $admin_permissions->filter(function ($permission) {
             $parent_permission_list = [
@@ -227,28 +227,22 @@ class PermissionRoleTableSeeder extends Seeder
                 'logbook_show',
                 'logbook_entry_access',
                 'logbook_entry_show',
-                'navigator_show', 
+                'navigator_show',
                 'task_access',
                 'task_show',
             ];
-            
+
             return in_array($permission->title, $parent_permission_list);
         });
         Role::findOrFail(7)->permissions()->sync($parent_permissions);
-        
+
         //set guest permissions
         $guest_permissions = $admin_permissions->filter(function ($permission) {
             $guest_permission_list = [
-                'curriculum_show', 
-                'logbook_access',
-                'logbook_show',
-                'logbook_entry_access',
-                'logbook_entry_show',
-                'navigator_show', 
-                'task_access',
-                'task_show',
+                'curriculum_show',
+                'navigator_show',
             ];
-            
+
             return in_array($permission->title, $guest_permission_list);
         });
         Role::findOrFail(8)->permissions()->sync($guest_permissions);

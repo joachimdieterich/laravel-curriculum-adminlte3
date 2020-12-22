105e597e547c1dbc1415419f08304d28fc6cb5ef
Joachim Dieterich <mail@joachimdieterich.de>
2020-12-22 15:23:16 +0100

    fixing test suites

--- tests/Feature/ManageOrganizationTest.php
+++ tests/Feature/ManageOrganizationTest.php
@@ -9,34 +9,41 @@ use App\Organization;
 class ManageOrganizationTest extends TestCase
 {
      use RefreshDatabase;
-     
+
      public function setUp(): void
     {
         parent::setUp();
         $this->signInAdmin();
     }
-    
-    
+
+
     /** @test */
     public function an_administrator_update_the_status_of_an_organization()
     {
-        
+
         /* add new organization */
         $this->post("/organizations" , $attributes = factory('App\Organization')->raw());
-        
+
         $this->assertDatabaseHas('organizations', $attributes);
-        
-        /* edit organization */ 
+
+        /* edit organization */
         $attributes['status_id'] = 2;
-        
+        $attributes['state_id'] = 'DE-RP';
+        $attributes['country_id'] = 'DE';
+
         $this->patch("/organizations/". (Organization::where(
-                        'title', '=', $attributes['title'])->first()->id) , 
-                        ['status_id' => 2] );
-        
+                        'title', '=', $attributes['title'])->first()->id) ,
+                        [
+                            'status_id' => 2,
+                            'state_id' => 'DE-RP',
+                            'country_id' => 'DE',
+                            'organization_type_id' => $attributes['organization_type_id']
+                        ] );
+
         $this->assertDatabaseHas('organizations', $attributes);
     }
-    
-    
-    
-    
+
+
+
+
 }

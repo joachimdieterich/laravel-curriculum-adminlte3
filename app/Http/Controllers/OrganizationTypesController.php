39acd4ae992dbb694fc9b16ce44faf82705c68b6
Joachim Dieterich <mail@joachimdieterich.de>
2020-12-23 14:34:02 +0100

    close #148: Organisation-Type: Error 500 if you edit organisation-types

--- app/Http/Controllers/OrganizationTypesController.php
+++ app/Http/Controllers/OrganizationTypesController.php
@@ -19,25 +19,25 @@ class OrganizationTypesController extends Controller
     {
         abort_unless(\Gate::allows('organization_type_access'), 403);
 
-        $organization_types = OrganizationType::all();   
+        $organization_types = OrganizationType::all();
 
         return view('organizationtypes.index', compact('organization_types'));
     }
-    
+
     public function list()
     {
         abort_unless(\Gate::allows('organization_type_access'), 403);
-  
+
         $organization_types = OrganizationType::select([
-            'id', 
-            'title', 
-            'external_id', 
-            'state_id', 
+            'id',
+            'title',
+            'external_id',
+            'state_id',
             'country_id']);
-        
+
         $edit_gate = \Gate::allows('organization_type_edit');
         $delete_gate = \Gate::allows('organization_type_delete');
-        
+
         return DataTables::of($organization_types)
             ->addColumn('action', function ($organization_types) use ($edit_gate, $delete_gate){
                  $actions  = '';
@@ -45,7 +45,7 @@ class OrganizationTypesController extends Controller
                         $actions .= '<a href="'.route('organizationtypes.edit', $organization_types->id).'" '
                                     . 'id="edit-organization-type-'.$organization_types->id.'" '
                                     . 'class="btn">'
-                                    . '<i class="fa fa-edit"></i>' 
+                                    . '<i class="fa fa-edit"></i>'
                                     . '</a>';
                     }
                     if ($delete_gate){
@@ -54,10 +54,10 @@ class OrganizationTypesController extends Controller
                                 . 'onclick="destroyDataTableEntry(\'organizationtypes\','.$organization_types->id.')">'
                                 . '<i class="fa fa-trash"></i></button>';
                     }
-              
+
                 return $actions;
             })
-            
+
             ->addColumn('check', '')
             ->setRowId('id')
             ->setRowAttr([
@@ -70,7 +70,7 @@ class OrganizationTypesController extends Controller
     {
         abort_unless(\Gate::allows('organization_type_create'), 403);
         $countries = Country::all();
-       
+
         $states = State::all()->sortBy('country');
         return view('organizationtypes.create')
                     ->with(compact('countries'))
@@ -80,17 +80,17 @@ class OrganizationTypesController extends Controller
     public function store()
     {
         abort_unless(\Gate::allows('organization_type_create'), 403);
-       
+
         $input = $this->validateRequest();
         $input_state = State::where('code', format_select_input($input['state_id']))->get()->first();
-       
+
         $organization_types = OrganizationType::create([
             'title'         => $input['title'],
             'external_id'   => $input['external_id'],
             'state_id'      => $input_state->code,
             'country_id'    =>  $input_state->country,
         ]);
-        
+
         return redirect()->route('organizationtypes.index');
     }
 
@@ -108,8 +108,14 @@ class OrganizationTypesController extends Controller
     public function update(OrganizationType $organizationtype)
     {
         abort_unless(\Gate::allows('organization_type_edit'), 403);
-       
-        $organizationtype->update($this->validateRequest());
+        $input = $this->validateRequest();
+        $input_state = State::where('code', format_select_input($input['state_id']))->get()->first();
+        $organizationtype->update([
+            'title'         => $input['title'],
+            'external_id'   => $input['external_id'],
+            'state_id'      => $input_state->code,
+            'country_id'    =>  $input_state->country,
+        ]);
 
         return redirect()->route('organizationtypes.index');
     }
@@ -134,9 +140,9 @@ class OrganizationTypesController extends Controller
         return back();
     }
 
-    
+
     protected function validateRequest()
-    {                      
+    {
         return request()->validate([
             'title'         => 'sometimes|required',
             'external_id'   => 'sometimes',

21941d9e4a13fe8a728cfb00a92c7ad4ca2d7f30
Joachim Dieterich <mail@joachimdieterich.de>
2021-01-23 21:09:02 +0100

    remove unused code

--- resources/js/app.js
+++ resources/js/app.js
@@ -91,18 +91,6 @@ Vue.prototype.$initTinyMCE = function (options) {
         file_browser_callback : function(field_name, url, type, win) {
             var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
             var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-
-            /*var cmsURL =  "/" + 'laravel-filemanager?field_name=' + field_name;
-                cmsURL = cmsURL + "&type=Files";
-
-            tinyMCE.activeEditor.windowManager.open({
-                file : cmsURL,
-                title : 'Filemanager',
-                width : x * 0.8,
-                height : y * 0.8,
-                resizable : "yes",
-                close_previous : "no"
-            });*/
         }
     });
 

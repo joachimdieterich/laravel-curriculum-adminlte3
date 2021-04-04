40ed7a28f37af680c3d82a2ac993c48118b1c653
Joachim Dieterich <mail@joachimdieterich.de>
2021-04-04 13:37:59 +0200

    adding admin view

---
+++ resources/views/admin/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+@section('title')
+     Administration
+@endsection
+@section('breadcrumb')
+    <li class="breadcrumb-item "><a href="/">{{ trans('global.home') }}</a></li>
+    <li class="breadcrumb-item active">Administration</li>
+    <li class="breadcrumb-item "><a href="/documentation" class="text-black-50"><i class="fas fa-question-circle"></i></a></li>
+@endsection
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <admin-view></admin-view>
+
+        <br>
+
+    </div>
+</div>
+@endsection

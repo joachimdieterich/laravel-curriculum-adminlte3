505c60013fd91500a0dd4933314ab709eea120b3
Joachim Dieterich <mail@joachimdieterich.de>
2021-04-05 13:18:24 +0200

    cleaning code

--- app/Http/Controllers/AchievementController.php
+++ app/Http/Controllers/AchievementController.php
@@ -19,43 +19,40 @@ class AchievementController extends Controller
     public function store(Request $request)
     {
         abort_unless(\Gate::allows('achievement_create'), 403);
-        
+
         $input = $this->validateRequest();
-       
+
         $user_ids = !empty($input['user_id']) ? $input['user_id']: auth()->user()->id;
-        
+
         foreach((array) $user_ids AS $user_id)
         {
             $achievement = Achievement::where('referenceable_type', '=', $input['referenceable_type'])
                                       ->where('referenceable_id', '=', $input['referenceable_id'])
                                       ->where('user_id', '=', $user_id)->first();
-            
+
                 $achievement = Achievement::updateOrCreate(
                     [
                         "referenceable_type" => $input['referenceable_type'],
                         "referenceable_id"   => $input['referenceable_id'],
-                        "user_id"            => $user_id,                        
+                        "user_id"            => $user_id,
                     ],
                     [
                         "status"             => $this->calculateStatus($user_id, $input, ($achievement === null) ? '00' : $achievement->status),
-                        "owner_id"           => auth()->user()->id,	
+                        "owner_id"           => auth()->user()->id,
                     ]
-                    
-                ); 
-            
-            $achievement->save(); 
-           
+                );
+
+            $achievement->save();
+
             $obj = EnablingObjective::find($input['referenceable_id']);
             (new ProgressController)->calculateProgress('App\TerminalObjective', $obj->terminal_objective_id, $user_id);
         }
-        // axios call? 
-        if (request()->wantsJson()){    
+        // axios call?
+        if (request()->wantsJson()){
             return ['message' => $achievement->status];
         }
         return $achievement;
     }
-    
-    
 
     /* calculate proper status id */
     protected function calculateStatus($user_id, $input, $status = '00')
@@ -70,10 +67,9 @@ class AchievementController extends Controller
         }
         return $status;
     }
-   
+
     protected function validateRequest()
-    {               
-        
+    {
         return request()->validate([
             'referenceable_type'    => 'required',
             'referenceable_id'      => 'required',

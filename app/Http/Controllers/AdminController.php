e3539bd6abbfc1306bce1b495a6e48f39761272e
Joachim Dieterich <mail@joachimdieterich.de>
2021-04-23 08:26:16 +0200

    change permission

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,7 +14,6 @@ class AdminController extends Controller
     public function index()
     {
         abort_unless(\Gate::allows('permission_create'), 403);
-        abort_unless((auth()->user()->id === 1), 403); //only side-admins
 
         return view('admin.show');//
     }

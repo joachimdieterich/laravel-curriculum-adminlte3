65c27acca2903be0c2042e60645c83c097545ec7
Joachim Dieterich <mail@joachimdieterich.de>
2022-03-19 08:22:37 +0100

    enhance code

--- resources/js/components/objectives/Header.vue
+++ resources/js/components/objectives/Header.vue
@@ -1,50 +1,52 @@
 <template>
-        <div class="boxheader row"
-             v-bind:style="{ 'color': textcolor }" >
-            <span class="mr-auto "
-                  v-can="'curriculum_edit'"
-                  v-if="edit_settings === true"
-                 >
-                <span v-if="(type == 'terminal' && objective.order_id != 0)"
-                       class="fa fa-arrow-up mr-1"
-                       @click="$emit('eventSort','-1')"></span>
-                <span v-if="(type == 'enabling'  && objective.order_id != 0)"
-                    class="fa fa-arrow-left mr-1 "
-                    @click="$emit('eventSort','-1')">
-                </span>
-                <span  v-if="(type == 'terminal' && max_id != objective.id)"
-                       class="fa fa-arrow-down  "
-                       @click="$emit('eventSort','1')"></span>
-                <span  v-if="(type == 'enabling' && max_id != objective.id)"
-                       class="fa fa-arrow-right  "
-                    @click="$emit('eventSort','1')">
-                </span>
+    <div class="boxheader row"
+         v-bind:style="{ 'color': textcolor }">
+        <span class="mr-auto "
+              v-can="'curriculum_edit'"
+              v-if="edit_settings === true">
+            <span v-if="(type == 'terminal' && objective.order_id != 0)"
+                  class="fa fa-arrow-up mr-1"
+                  @click="$emit('eventSort','-1')">
             </span>
-            <span  v-if="(type == 'enabling'  && objective.level != null)" >
-                <button type="button"
-                        class="btn btn-block  btn-xs"
-                        v-bind:class="objective.level.css_color"
-                        v-html="objective.level.title">
-                </button>
+            <span v-if="(type == 'enabling'  && objective.order_id != 0)"
+                  class="fa fa-arrow-left mr-1 "
+                  @click="$emit('eventSort','-1')">
             </span>
-            <span  class="ml-auto"
-                   v-can="'curriculum_edit'">
-                <DropdownButton v-if="type == 'terminal'"
-                                @eventDelete="emitDeleteEvent"
-                                :menuEntries="menuEntries"
-                                :objective.sync="objective"
-                                :textcolor="textcolor"/>
-                <DropdownButton v-else-if="type == 'enabling'"
-                                @eventDelete="emitDeleteEvent"
-                                :menuEntries="menuEntries"
-                                :objective.sync="objective"
-                                :textcolor="textcolor"/>
+            <span v-if="(type == 'terminal' && max_id != objective.id)"
+                  class="fa fa-arrow-down  "
+                  @click="$emit('eventSort','1')">
             </span>
+            <span v-if="(type == 'enabling' && max_id != objective.id)"
+                  class="fa fa-arrow-right  "
+                  @click="$emit('eventSort','1')">
+            </span>
+        </span>
 
-        </div>
+        <span v-if="(type == 'enabling'  && objective.level != null)">
+            <button type="button"
+                    class="btn btn-block  btn-xs"
+                    v-bind:class="objective.level.css_color"
+                    v-html="objective.level.title">
+            </button>
+        </span>
 
+        <span
+            v-can="'curriculum_edit'"
+            v-if="edit_settings === true"
+            class="ml-auto">
+            <DropdownButton v-if="type == 'terminal'"
+                            @eventDelete="emitDeleteEvent"
+                            :menuEntries="menuEntries"
+                            :objective.sync="objective"
+                            :textcolor="textcolor"/>
+            <DropdownButton v-else-if="type == 'enabling'"
+                            @eventDelete="emitDeleteEvent"
+                            :menuEntries="menuEntries"
+                            :objective.sync="objective"
+                            :textcolor="textcolor"/>
+        </span>
+    </div>
 </template>
-
 
 <script>
     import DropdownButton from './DropdownButton'

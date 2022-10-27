4cd4ccf3f9a79969759792d4cc28d6c875b2d5ee
Joachim Dieterich <mail@joachimdieterich.de>
2022-10-27 10:53:32 +0200

    fix webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,28 +15,31 @@ mix.js(['resources/js/app.js', 'vendor/select2/select2/dist/js/select2.min.js'],
    .js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
    .sass('resources/sass/app.scss', 'public/css')
 
-mix.copyDirectory('node_modules/datatables.net-buttons/js', 'public/js/datatables.net-buttons/');
+
 
 /* TinyMCE */
+mix.copyDirectory('node_modules/tinymce/plugins', 'public/js/plugins');
 mix.copyDirectory('node_modules/tinymce/plugins', 'public/js/plugins');
 mix.copyDirectory('node_modules/tinymce/skins', 'public/js/skins');
 mix.copyDirectory('node_modules/tinymce/themes', 'public/js/themes');
 mix.copyDirectory('node_modules/tinymce/icons', 'public/js/icons');
+mix.copyDirectory('node_modules/tinymce', 'public/node_modules/tinymce');
 
 /* Bootstrap colorpicker */
-//mix.copyDirectory('node_modules/bootstrap-colorpicker/dist', 'public/node_modules/bootstrap-colorpicker');
+mix.copyDirectory('node_modules/bootstrap-colorpicker/dist', 'public/node_modules/bootstrap-colorpicker');
 /* Bootstrap datetimepicker */
-//mix.copyDirectory('node_modules/@activix/bootstrap-datetimepicker/js', 'public/node_modules/bootstrap-datetimepicker');
+mix.copyDirectory('node_modules/@activix/bootstrap-datetimepicker/js', 'public/node_modules/bootstrap-datetimepicker');
 /* Datatables */
-/*mix.copyDirectory('node_modules/datatables.net/js', 'public/node_modules/datatables.net/js');
+mix.copyDirectory('node_modules/datatables.net/js', 'public/node_modules/datatables.net/js');
 mix.copyDirectory('node_modules/datatables.net-bs4/js', 'public/node_modules/datatables.net-bs4/js');
+mix.copyDirectory('node_modules/datatables.net-buttons', 'public/node_modules/datatables.net-buttons/');
 
-mix.copyDirectory('node_modules/datatables.net-select/js', 'public/node_modules/datatables.net-select/js');*/
+mix.copyDirectory('node_modules/datatables.net-select/js', 'public/node_modules/datatables.net-select/js');
 /* moment */
-//mix.copyDirectory('node_modules/moment/min', 'public/node_modules/moment/js');
+mix.copyDirectory('node_modules/moment/min', 'public/node_modules/moment/js');
 /* mathjax */
-/*mix.copyDirectory('node_modules/mathjax/es5', 'public/node_modules/mathjax/es5');
-mix.copyDirectory('node_modules/@dimakorotkov/tinymce-mathjax', 'public/node_modules/@dimakorotkov/tinymce-mathjax');*/
+mix.copyDirectory('node_modules/mathjax/es5', 'public/node_modules/mathjax/es5');
+mix.copyDirectory('node_modules/@dimakorotkov/tinymce-mathjax', 'public/node_modules/@dimakorotkov/tinymce-mathjax');
 
 mix.version();
 mix.vue({version: 2});

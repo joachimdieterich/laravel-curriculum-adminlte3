f1875332b7af1dc8e944822304a7dc3739d16c1c
Joachim Dieterich <mail@joachimdieterich.de>
2022-10-31 09:19:57 +0100

    fix condidtion

--- resources/js/components/kanban/KanbanItem.vue
+++ resources/js/components/kanban/KanbanItem.vue
@@ -137,7 +137,8 @@ export default {
   },
     computed:{
         textColor: function(){
-          if(this.item.color == "" || this.item.color != null) return
+          if(this.item.color != null) return;
+          if(this.item.color == "" ) return;
           let hex = this.item.color.substring(1, 7);
           let r = parseInt(hex.slice(0, 2), 16),
               g = parseInt(hex.slice(2, 4), 16),

34e087a6415436d628001231246e7edd03ac00b9
Joachim Dieterich <mail@joachimdieterich.de>
2022-11-24 08:37:16 +0100

    adding public parameter

--- resources/js/components/media/MediumModal.vue
+++ resources/js/components/media/MediumModal.vue
@@ -62,6 +62,7 @@
                          <textarea
                              style="width:100%"
                              v-model="medium.description"/>
+                    <input type="checkbox"/>
                 </div>
                 <div v-else-if="medium.description != ''"
                     v-html="medium.description"

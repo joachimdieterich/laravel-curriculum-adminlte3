925df1246a6f3a4f3bb374ed52de14b96512c204
joachimdieterich <mail@curriculumonline.de>
2023-01-25 17:39:34 +0100

    Enhance Config

--- config/medium.php
+++ config/medium.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Interfaces\Implementations\LocalMediaAdapter;
-use App\Plugins\Repositories\edusharing\EdusharingMediaAdapter;
-
 return [
 
     /*
@@ -19,11 +16,11 @@ return [
         'default' => env('MEDIA_ADAPTER', 'local'),
 
         'local' => [
-            'adapter' => new LocalMediaAdapter(),
+            'adapter' => 'LocalMediaAdapter',
         ],
 
         'edusharing' => [
-            'adapter' => new EdusharingMediaAdapter(),
+            'adapter' => 'EdusharingMediaAdapter',
             'upload_iframe_url' => env('EDUSHARING_UPLOAD_IFRAME_URL', ''),
             'cloud_iframe_url' => env('EDUSHARING_CLOUD_IFRAME_URL', ''),
         ],

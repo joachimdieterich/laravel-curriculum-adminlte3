a79002f2b0bf4642bd47950ebb6df606ada99d00
alexander.lang <alexander.lang@pl.rlp.de>
2023-05-03 13:43:38 +0200

    first commit

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Map;
+use Illuminate\Http\Request;
+
+class MapController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        abort_unless(\Gate::allows('map_access'), 403);
+
+        return view('map.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Map $map)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Map $map)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Map $map)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Map $map)
+    {
+        //
+    }
+}

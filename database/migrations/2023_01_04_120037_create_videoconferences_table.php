9b2d5624ad131ffa7b5efa20de47482627118558
joachimdieterich <mail@curriculumonline.de>
2023-10-09 19:26:09 +0200

    Adding videoconference migrations

--- database/migrations/2023_01_04_120037_create_videoconferences_table.php
+++ database/migrations/2023_01_04_120037_create_videoconferences_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('meetingName');
             $table->string('attendeePW');
             $table->string('moderatorPW');
-            $table->string('callbackUrl');
+            $table->string('endCallbackUrl');
 
             $table->unsignedbigInteger('owner_id');
 

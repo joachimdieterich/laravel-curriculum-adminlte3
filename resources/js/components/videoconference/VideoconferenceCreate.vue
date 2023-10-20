96556158074a7e930989f934cb1ca753bb60f9af
alexander.lang <alexander.lang@pl.rlp.de>
2023-10-20 15:01:09 +0200

    added datatable to videoconferences

--- resources/js/components/videoconference/VideoconferenceCreate.vue
+++ resources/js/components/videoconference/VideoconferenceCreate.vue
@@ -163,7 +163,7 @@ export default {
                 'lockSettingsLockOnJoin': false,
                 'lockSettingsLockOnJoinConfigurable': false,
                 'guestPolicy': 'ALWAYS_ACCEPT',
-                'userName': '',
+                'userName': '', // TODO: where is this needed? | not in db
                 'meetingKeepEvents': false,
                 'endWhenNoModerator': false,
                 'endWhenNoModeratorDelayInMinutes': 1,

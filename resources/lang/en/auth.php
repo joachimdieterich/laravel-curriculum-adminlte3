0890c8377f4d1606c80e30dd6271bb258b90e086
Alexander Lang <alexander.lang@pl.rlp.de>
2024-01-16 07:52:51 +0100

    password reset can now be toggled per .env variable

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'or' => 'OR',
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'site_title' => 'curriculum',

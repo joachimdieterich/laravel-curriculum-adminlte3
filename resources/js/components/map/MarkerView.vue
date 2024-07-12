2319b825062f537d897153c95396944e68b96e1f
Alexander Lang <alexanderlang1.al+github@gmail.com>
2024-07-12 14:01:16 +0200

    hack to hide media only if there's no media attached

--- resources/js/components/map/MarkerView.vue
+++ resources/js/components/map/MarkerView.vue
@@ -28,17 +28,17 @@
             v-html="marker.description"
         ></div>
 
-        <!-- <h5 class="pt-3">{{ trans('global.media.title') }}</h5>
-        <div v-if="marker.id != null"
+        <h5 class="pt-3" :class="!show_media && 'd-none'">{{ trans('global.media.title') }}</h5>
+        <div v-if="marker.id != null && show_media"
              v-bind:id="'map_marker_media_'+marker.id"
         >
             <media
                 subscribable_type="App\MapMarker"
-               :subscribable_id="marker.id"
+                :subscribable_id="marker.id"
                 :can_add_media="$userId == marker.owner_id || $userId == map.owner_id"
-               format="list"
+                format="list"
             />
-        </div> -->
+        </div>
 
         <h5 v-if="marker.address !== ''" class="pt-3">{{ trans('global.marker.fields.address') }}</h5>
         <div v-html="marker.address"></div>
@@ -55,13 +55,13 @@
     </div>
 </template>
 <script>
-// import Media from '../media/Media';
+import Media from '../media/Media';
 
 export default {
     name: 'MarkerView',
-    // components: {
-    //     Media,
-    // },
+    components: {
+        Media,
+    },
     props: {
         marker: {
             default: null
@@ -74,16 +74,22 @@ export default {
         return {
             component_id: this._uid,
             tag_array: {},
+            show_media: true,
         }
     },
     watch: { // reload if context change
         marker: function(newVal, oldVal) {
             this.tag_array = newVal.tags?.split(",");
+            this.show_media = true;
         },
     },
     methods: {
         editMarker(marker){
             this.$eventHub.$emit('edit_marker', marker);
+        },
+        hideMedia() {
+            if (this.$userId == this.marker.owner_id || this.$userId == this.map.owner_id) return;
+            this.show_media = false;
         },
     },
 }

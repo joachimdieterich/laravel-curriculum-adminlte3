d9d627e6052befcd104351e192b5380385bfade9
Alexander Lang <alexanderlang1.al+github@gmail.com>
2024-09-25 11:48:30 +0200

    Plan: fixed tooltip-hover

--- resources/js/components/plan/Plan.vue
+++ resources/js/components/plan/Plan.vue
@@ -27,7 +27,7 @@
                             <span class="tooltip-container">
                                 <span class="tooltip-wrapper">
                                     <span class="tooltip-text">
-                                        {{ mode_toggle ? 'Kompetenz anklicken um mehrere Personen einzuschätzen' : 'Einschätzungen für einzelne Person abgeben' }}
+                                        {{ mode_toggle ? 'Kompetenz anklicken um mehrere Personen einzuschätzen' : 'Person auswählen und direkt einschätzen' }}
                                     </span>
                                 </span>
                                 <a class="link-muted" style="padding-left: 6px; margin-right: -4px">
@@ -328,7 +328,7 @@ export default {
             }
         }
     }
-    &:hover .tooltip-wrapper .tooltip-text { visibility: visible; }
+    &:hover .tooltip-wrapper:not(:hover) .tooltip-text { visibility: visible; }
 }
 #corner-button {
     color: white;

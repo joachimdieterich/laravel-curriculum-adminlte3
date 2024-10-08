4256c9af57a4fda1010cd22776804be375ac6352
Alexander Lang <alexander.lang@pl.rlp.de>
2024-10-08 10:06:02 +0200

    objective: added hidden button for edusharing-nextPage testing

--- app/Plugins/Repositories/edusharing/resources/js/components/Media.vue
+++ app/Plugins/Repositories/edusharing/resources/js/components/Media.vue
@@ -141,6 +141,7 @@
                </span>
             </span>
         </div>
+        <button class="d-none" @click="test()">+</button>
     </div>
 </template>
 
@@ -207,6 +208,22 @@ export default {
             }
 
             $("#loading").hide();
+        },
+        // JUST FOR TESTING
+        test() {
+            this.page++;
+            
+            this.externalMedia = (axios.get('/repositorySubscriptions/getMedia', {
+                params: {
+                    subscribable_type: this.subscribable_type(),
+                    subscribable_id: this.model.id,
+                    search: this.model.title,
+                    page: this.page,
+                    maxItems: this.maxItems,
+                    repository: 'edusharing',
+                    filter: 1,
+                },
+            })).data.message[0];
         },
         async unlinkMedium(id, value) { //id of external reference and value in db
             try {

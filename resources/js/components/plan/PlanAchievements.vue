c95a17d6f8106c7871b1a2f28ea18141679be5ed
Alexander Lang <alexander.lang@pl.rlp.de>
2024-10-10 11:46:27 +0200

    closed #556 | restructured PlanAchievements

--- resources/js/components/plan/PlanAchievements.vue
+++ resources/js/components/plan/PlanAchievements.vue
@@ -1,142 +1,130 @@
 <template>
     <div id="show-achievements">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Ziele / Namen</th>
-                </tr>
-            </thead>
-            <tbody></tbody>
-        </table>
+        <div id="header" class="d-flex w-100">
+            <span class="flex-fill">Ziele / Namen</span>
+            <span v-for="user in users"
+                class="flex-fill text-center"
+            >
+                {{ user.firstname }} {{ user.lastname }}
+            </span>
+        </div>
+        <div id="achievements">
+            <div v-for="ter in objectives">
+                <div
+                    class="terminal pointer collapsed"
+                    data-toggle="collapse"
+                    :data-target="'#terminal_' + ter.terminal_objective.id"
+                    aria-expanded="true"
+                >
+                    <span class="float-left mr-1" v-html="ter.terminal_objective.title"></span>
+                    <span class="fa fa-angle-up"></span>
+                </div>
+                <div :id="'terminal_' + ter.terminal_objective.id" class="collapse show">
+                    <div v-for="ena in ter.terminal_objective.enabling_objectives"
+                        class="d-flex w-100 enabling"
+                    >
+                        <span
+                            class="flex-fill pl-2"
+                            v-html="ena.title"
+                        ></span>
+                        <span v-for="(user, index) in users"
+                            class="flex-fill"
+                        >
+                            <span v-if="ena.achievements.length === users.length || ena.achievements.length === 0"
+                                class="d-flex justify-content-center align-items-center h-100"
+                                :class="'status-' + (ena.achievements[index]?.status[1] ?? '0')"
+                            >
+                                <i class="fa fa-circle"></i>
+                            </span>
+                            <span v-else
+                                class="d-flex justify-content-center align-items-center h-100"
+                                :class="'status-' + (ena.achievements.find(ach => ach.user_id === user.id)?.status[1] ?? '0')"
+                            >
+                                <i class="fa fa-circle"></i>
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
     props: {
-        terminal: {},
-        enabling: {},
+        terminal: [],
+        enabling: [],
         users: [],
     },
-    mounted() {
-        const achievements = {
-            terminal: this.terminal,
-            enabling: this.enabling,
-        };
-
-        const table = document.querySelector('#show-achievements .table');
-        const thead = table.firstChild;
-        const tbody = table.lastChild;
-
-        // list all names in the table-header
-        this.users.forEach(user => {
-            const th = document.createElement('th');
-            th.innerText = user.firstname + ' ' + user.lastname;
-            th.style.width = 'calc(100% / ' + (this.users.length + 1) + ')';
-            thead.firstChild.appendChild(th);
-        });
-        // list each terminal-objective-title in its own row
-        achievements.terminal.forEach(ter => {
-            const terminal_tr = document.createElement('tr');
-            const terminal_td = document.createElement('td');
-
-            terminal_tr.id = 'terminal_' + ter.terminal_objective_id;
-            terminal_tr.classList.add('thick-line');
-            terminal_td.innerHTML = ter.terminal_objective.title;
-
-            terminal_tr.appendChild(terminal_td);
-            tbody.appendChild(terminal_tr);
-
-            // list each enabling-objective with its given achievement status for the corresponding user
-            ter.terminal_objective.enabling_objectives.forEach(ena => {
-                const enabling_tr = document.createElement('tr');
-                const enabling_td = document.createElement('td');
-
-                enabling_td.innerHTML = ena.title;
-                enabling_tr.appendChild(enabling_td);
-
-                this.addAchievementCells(enabling_tr, ena.achievements);
-
-                tbody.appendChild(enabling_tr);
-            })
-        });
-
-        achievements.enabling.forEach(ena => {
-            let terminal_tr = document.getElementById('terminal_' + ena.enabling_objective.terminal_objective_id);
-            const enabling_tr = document.createElement('tr');
-            const enabling_td = document.createElement('td');
-
-            // if terminal-objective doesn't exist, create a new terminal-row
-            if (terminal_tr === null) {
-                const tr = document.createElement('tr');
-                const td = document.createElement('td');
-
-                tr.id = 'terminal_' + ena.enabling_objective.terminal_objective_id;
-                tr.classList.add('thick-line');
-                td.innerHTML = ena.enabling_objective.terminal_objective.title;
-
-                tr.appendChild(td);
-                tbody.appendChild(tr);
-
-                terminal_tr = tr; // save newly created terminal-tr to append the enabling-tr
-            }
-            
-            // create an enabling-row
-            enabling_td.innerHTML = ena.enabling_objective.title;
-            enabling_tr.appendChild(enabling_td);
-
-            this.addAchievementCells(enabling_tr, ena.enabling_objective.achievements);
-
-            // find its last enabling-objective tr
-            const last_enabling =
-                $(terminal_tr).closest('tr').nextAll('.thick-line')[0]?.previousElementSibling // search next terminal-tr
-                ?? terminal_tr.parentElement.lastElementChild; // if there's no next terminal-tr, get last tr of table
-            // and add the new enabling-row
-            last_enabling.after(enabling_tr);
-        });
+    data() {
+        return {
+            objectives: [],
+        }
     },
-    methods: {
-        addAchievementCells(tr, achievements) {
-            this.users.forEach(user => {
-                const td = document.createElement('td');
-                const i = document.createElement('i');
-                let status = '0';
+    mounted() {
+        let obj = [];
+        // preset needed attributes to terminal-objective placeholder
+        let ter = { terminal_objective: this.enabling[0]?.enabling_objective.terminal_objective };
+        let objectives = [];
 
-                if (achievements.length > 0) {
-                    // find out if there's a set achievement and get the teacher's value | if not => 0
-                    status = achievements.find(achievement => achievement.user_id === user.id)?.status[1] ?? '0';
-                }
-
-                td.classList.add('status-' + status);
-                i.classList.add('fa', 'fa-circle');
-
-                td.appendChild(i);
-                tr.appendChild(td);
-            });
-        },
+        this.enabling.forEach(ena => {
+            const ena_obj = ena.enabling_objective;
+            
+            // if current enabling-objective has the same terminal-objective as previous
+            if (ena_obj.terminal_objective_id === ter.terminal_objective.id) {
+                objectives.push(ena_obj);
+            } else { // push placeholder to obj and preset attributes again
+                ter.terminal_objective.enabling_objectives = objectives;
+                obj.push(structuredClone(ter));
+                ter.terminal_objective = ena_obj.terminal_objective;
+                objectives = [ena_obj];
+            }
+        });
+        // if there's no enabling-objective, this value will be undefined
+        if (ter.terminal_objective !== undefined) {
+            ter.terminal_objective.enabling_objectives = objectives;
+            obj.push(ter);
+        }
+        this.objectives = this.terminal.concat(obj);
     },
 }
 </script>
 <style scoped>
 #show-achievements {
-    & >>> p { margin: 0px; color: black; }
-    & >>> td { padding: 10px; vertical-align: middle; }
-    & >>> td:not(:first-child) { text-align: center; font-size: 1.5rem; }
-    & >>> tr:hover :not(.status) { background-color: unset; }
-    & >>> tr:first-child,
-    & >>> tr:first-child th { border-top: 0px; }
-    & >>> th {
-        font-size: 1.25rem;
-        text-align: center;
+    & #header {
+        padding: 8px 0px;
 
-        &:first-child { min-width: 15%; max-width: 25%; text-align: left; }
+        & > span {
+            font-size: 1.25rem;
+            font-weight: 700;
+            min-width: 17%;
+    
+            &:first-child { min-width: 15%; max-width: 25%; }
+        }
     }
-    & >>> .thick-line {
-        border-top: 3px solid #dee2e6;
-        border-bottom: 3px solid #dee2e6;
+    & #achievements {
+        & .terminal {
+            padding: 8px 0px;
+            font-size: 1.05rem;
+            border-top: 3px solid #dee2e6;
+            border-bottom: 3px solid #dee2e6;
+
+            &:hover { background-color: #e9ecef; }
+            & > .fa { transition: 0.3s transform; }
+            &:not(.collapsed) > .fa { transform: rotate(-180deg); }
+        }
+        & .enabling {
+            padding: 10px 0px;
+
+            &:not(:first-child) { border-top: 1px solid #dee2e6; }
+            & > :first-child { min-width: 15%; max-width: 25%; }
+            & .fa { font-size: 1.5rem; }
+            & .status-0 { color: #d2d6de !important; }
+            & .status-1 { color: #00a65a !important; }
+            & .status-2 { color: #fd7e14 !important; }
+            & .status-3 { color: #dd4b39 !important; }
+        }
     }
-    & >>> .status-0 { color: #d2d6de !important; }
-    & >>> .status-1 { color: #00a65a !important; }
-    & >>> .status-2 { color: #fd7e14 !important; }
-    & >>> .status-3 { color: #dd4b39 !important; }
+
 }
 </style>

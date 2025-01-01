8836a72cdf0527eef208cb5f85476923440cf8c5
joachimdieterich <mail@curriculumonline.de>
2025-01-01 18:37:47 +0100

    Refactor VariantDefinition to v11

---
+++ resources/js/components/variantDefinition/VariantDefinitionModal.vue
@@ -0,0 +1,217 @@
+<template>
+    <Transition name="modal">
+        <div v-if="globalStore.modals[$options.name]?.show"
+             class="modal-mask"
+        >
+        <div class="modal-container">
+            <div class="card-header">
+                <h3 class="card-title">
+                    <span v-if="method === 'post'">
+                        {{ trans('global.variantDefinition.create') }}
+                    </span>
+                    <span v-if="method === 'patch'">
+                        {{ trans('global.variantDefinition.edit') }}
+                    </span>
+                </h3>
+                <div class="card-tools">
+                    <button type="button"
+                            class="btn btn-tool"
+                            @click="globalStore?.closeModal($options.name)">
+                        <i class="fa fa-times"></i>
+                    </button>
+                </div>
+            </div>
+            <form >
+                <div class="card-body" style="max-height: 80vh; overflow-y: auto;">
+                    <div class="form-group "
+                        :class="form.errors.title ? 'has-error' : ''"
+                          >
+                        <label for="title">
+                            {{ trans('global.variantDefinition.fields.title') }} *
+                        </label>
+                        <input
+                            type="text" id="title"
+                            name="title"
+                            class="form-control"
+                            v-model="form.title"
+                            placeholder="Title"
+                            required
+                            />
+                         <p class="help-block"
+                            v-if="form.errors.title"
+                            v-text="form.errors.title[0]"></p>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description">
+                            {{ trans('global.variantDefinition.description') }}
+                        </label>
+                        <Editor
+                            id="description"
+                            name="description"
+                            class="form-control"
+                            :init="tinyMCE"
+                            v-model="form.description"
+                        />
+                        <p class="help-block"
+                           v-if="form.errors.description"
+                           v-text="form.errors.description[0]"
+                        ></p>
+                    </div>
+
+                    <v-swatches
+                        :swatch-size="49"
+                        :trigger-style="{}"
+                        popover-to="right"
+                        v-model="this.form.color"
+
+                        show-fallback
+                        fallback-input-type="color"
+
+                        @input="(id) => {
+                                    if (id.isInteger){
+                                      this.form.color = id;
+                                    }
+                                }"
+                        :max-height="300"
+                    ></v-swatches>
+
+                    <div class="dropdown">
+                        <button
+                            class="btn btn-default"
+                            style="width: 42px; padding: 6px 0px;"
+                            type="button"
+                            data-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            <i :class="form.css_icon + ' pt-2'"></i>
+                        </button>
+                        <font-awesome-picker
+                            class="dropdown-menu dropdown-menu-right"
+                            style="min-width: 400px;"
+                            :searchbox="trans('global.select_icon')"
+                            v-on:selectIcon="setIcon"
+                        ></font-awesome-picker>
+                    </div>
+
+                </div>
+                <div class="card-footer">
+                     <span class="pull-right">
+                         <button
+                             id="objective-cancel"
+                             type="button"
+                             class="btn btn-default"
+                             @click="globalStore?.closeModal($options.name)">
+                             {{ trans('global.cancel') }}
+                         </button>
+                         <button
+                             id="objective-save"
+                             class="btn btn-primary"
+                             @click="submit(method)" >
+                             {{ trans('global.save') }}
+                         </button>
+                    </span>
+                </div>
+            </form>
+        </div>
+    </div>
+    </Transition>
+</template>
+<script>
+import Form from 'form-backend-validation';
+import Editor from '@tinymce/tinymce-vue';
+import {useGlobalStore} from "../../store/global";
+import FontAwesomePicker from "../../../views/forms/input/FontAwesomePicker.vue";
+
+export default {
+    name: 'variant-definition-modal',
+    components: {
+        FontAwesomePicker,
+        Editor
+    },
+    props: {},
+    setup () {
+        const globalStore = useGlobalStore();
+        return {
+            globalStore,
+        }
+    },
+    data() {
+        return {
+            component_id: this.$.uid,
+            method: 'post',
+            url: '/variantDefinitions',
+            form: new Form({
+                'id':'',
+                'title': '',
+                'description': '',
+                'color':'#27AF60',
+                'css_icon': 'fa fa-book',
+            }),
+            tinyMCE: this.$initTinyMCE(
+                [
+                    "autolink link curriculummedia"
+                ],
+                {
+                    'eventHubCallbackFunction': 'insertContent',
+                    'eventHubCallbackFunctionParams': this.component_id,
+                }
+            ),
+            search: '',
+        }
+    },
+    methods: {
+        async submit(method) {
+            try {
+                if (method === 'patch') {
+                    this.update();
+                } else {
+                    this.add();
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        add() {
+            axios.post(this.url, this.form)
+                .then(r => {
+                    this.$eventHub.emit('variant-definition-added', r.data);
+                })
+                .catch(e => {
+                    console.log(e.response);
+                });
+        },
+        update() {
+            axios.patch(this.url + '/' + this.form.id, this.form)
+                .then(r => {
+                    this.$eventHub.emit('variant-definition-updated', r.data);
+                })
+                .catch(e => {
+                    console.log(e.response);
+                });
+        },
+        setIcon(selectedIcon) {
+            this.form.css_icon = 'fa fa-' + selectedIcon.className;
+        },
+    },
+    mounted() {
+        this.globalStore.registerModal(this.$options.name);
+        this.globalStore.$subscribe((mutation, state) => {
+            if (state.modals[this.$options.name].show) {
+                const params = state.modals[this.$options.name].params;
+                this.form.reset();
+                if (typeof (params) !== 'undefined') {
+                    this.form.populate(params);
+
+                    this.form.description = this.htmlToText(params.description);
+                    if (this.form.id !== '') {
+                        this.method = 'patch';
+                    } else {
+                        this.method = 'post';
+                    }
+                }
+            }
+        });
+    },
+}
+</script>
---
+++ resources/js/components/variantDefinition/VariantDefinitions.vue
@@ -0,0 +1,186 @@
+<template >
+    <div class="row">
+        <div id="variant-definition-content"
+             class="col-md-12 m-0">
+            <IndexWidget
+                v-permission="'organization_type_create'"
+                key="'variantDefinitionCreate'"
+                modelName="VariantDefinition"
+                url="/variantDefinitions"
+                :create=true
+                :createLabel="trans('global.variantDefinition.create')">
+            </IndexWidget>
+            <IndexWidget
+                v-for="variantDefinition in variantDefinitions"
+                :key="'variantDefinitionIndex'+variantDefinition.id"
+                :model="variantDefinition"
+                modelName= "VariantDefinition"
+                url="/variantDefinitions">
+                <template v-slot:icon>
+                    <i class="fa fa-university pt-2"></i>
+                </template>
+
+                <template
+                    v-permission="'organization_type_edit, organization_type_delete'"
+                    v-slot:dropdown>
+                    <div class="dropdown-menu dropdown-menu-right"
+                         style="z-index: 1050;"
+                         x-placement="left-start">
+                        <button
+                            v-permission="'organization_type_edit'"
+                            :name="'edit-variant-definition-' + variantDefinition.id"
+                            class="dropdown-item text-secondary"
+                            @click.prevent="editVariantDefinition(variantDefinition)">
+                            <i class="fa fa-pencil-alt mr-2"></i>
+                            {{ trans('global.variantDefinition.edit') }}
+                        </button>
+                        <hr class="my-1">
+                        <button
+                            v-permission="'organization_type_delete'"
+                            :id="'delete-variant-definition-' + variantDefinition.id"
+                            type="submit"
+                            class="dropdown-item py-1 text-red"
+                            @click.prevent="confirmItemDelete(variantDefinition)">
+                            <i class="fa fa-trash mr-2"></i>
+                            {{ trans('global.variantDefinition.delete') }}
+                        </button>
+                    </div>
+                </template>
+            </IndexWidget>
+        </div>
+        <div id="variant-definition-datatable-wrapper"
+             class="w-100 dataTablesWrapper">
+            <DataTable
+                id="variant-definition-datatable"
+                :columns="columns"
+                :options="options"
+                :ajax="url"
+                :search="search"
+                width="100%"
+                style="display:none; "
+            ></DataTable>
+        </div>
+
+        <Teleport to="body">
+            <VariantDefinitionModal></VariantDefinitionModal>
+            <ConfirmModal
+                :showConfirm="this.showConfirm"
+                :title="trans('global.variantDefinition.delete')"
+                :description="trans('global.variantDefinition.delete_helper')"
+                @close="() => {
+                    this.showConfirm = false;
+                }"
+                @confirm="() => {
+                    this.showConfirm = false;
+                    this.destroy();
+                }"
+            ></ConfirmModal>
+        </Teleport>
+    </div>
+</template>
+
+
+<script>
+import VariantDefinitionModal from "../variantDefinition/VariantDefinitionModal.vue";
+import IndexWidget from "../uiElements/IndexWidget.vue";
+import DataTable from 'datatables.net-vue3';
+import DataTablesCore from 'datatables.net-bs5';
+import ConfirmModal from "../uiElements/ConfirmModal.vue";
+import {useGlobalStore} from "../../store/global";
+DataTable.use(DataTablesCore);
+
+export default {
+    props: {
+
+    },
+    setup () {
+        const globalStore = useGlobalStore();
+        return {
+            globalStore,
+        }
+    },
+    data() {
+        return {
+            component_id: this.$.uid,
+            variantDefinitions: null,
+            search: '',
+            url: '/variantDefinitions/list',
+            errors: {},
+            currentVariantDefinition: {},
+            showConfirm: false,
+            columns: [
+                { title: 'id', data: 'id' },
+                { title: 'title', data: 'title', searchable: true},
+                { title: 'description', data: 'description', searchable: true},
+                { title: 'color', data: 'color', searchable: true},
+                { title: 'css_icon', data: 'css_icon', searchable: true},
+            ],
+            options : this.$dtOptions,
+            modalMode: 'edit'
+        }
+    },
+    mounted() {
+        this.$eventHub.emit('showSearchbar', true);
+
+        this.loaderEvent();
+
+        this.$eventHub.on('variantDefinition-added', (variantDefinition) => {
+            this.loaderEvent();
+            this.globalStore?.closeModal('variant-definition-modal');
+        });
+
+        this.$eventHub.on('variantDefinition-updated', (variantDefinition) => {
+            this.globalStore?.closeModal('variant-definition-modal');
+        });
+        this.$eventHub.on('createVariantDefinition', () => {
+            this.globalStore?.showModal('variant-definition-modal', {});
+        });
+    },
+    methods: {
+        editVariantDefinition(variantDefinition){
+            this.currentVariantDefinition = variantDefinition;
+            this.globalStore?.showModal('variant-definition-modal', this.currentVariantDefinition);
+        },
+        loaderEvent(){
+            const dt = $('#variant-definition-datatable').DataTable();
+            dt.on('draw.dt', () => { // checks if the datatable-data changes, to update the curriculum-data
+                this.variantDefinitions = dt.rows({page: 'current'}).data().toArray();
+
+                $('#variant-definition-content').insertBefore('#variant-definition-datatable-wrapper');
+            });
+            this.$eventHub.on('filter', (filter) => {
+                dt.search(filter).draw();
+            });
+        },
+        confirmItemDelete(variantDefinition){
+            this.showConfirm = true;
+            this.currentVariantDefinition = variantDefinition;
+        },
+        destroy() {
+            axios.delete('/variantDefinitions/' + this.currentVariantDefinition.id)
+                .then(res => {
+                    let index = this.variantDefinitions.indexOf(this.currentVariantDefinition);
+                    this.variantDefinitions.splice(index, 1);
+                })
+                .catch(err => {
+                    console.log(err.response);
+                });
+        },
+        update(variantDefinition) {
+            const index = this.variantDefinitions.findIndex(
+                vc => vc.id === variantDefinition.id
+            );
+
+            for (const [key, value] of Object.entries(variantDefinition)) {
+                this.organizations[index][key] = value;
+            }
+        }
+    },
+    components: {
+        ConfirmModal,
+        DataTable,
+        VariantDefinitionModal,
+        IndexWidget
+    },
+}
+</script>

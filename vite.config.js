56243c30a72c219c06c2938211cfa16c48d976ed
joachimdieterich <mail@curriculumonline.de>
2025-01-12 17:20:16 +0100

    remove vite-plugin-static-copy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import {viteStaticCopy} from "vite-plugin-static-copy";
 import vue from '@vitejs/plugin-vue';
 import commonjs from 'vite-plugin-commonjs';
 import mix from "laravel-mix";
@@ -10,14 +9,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],
-        }),
-        viteStaticCopy({ // !! only works with npm run build (not dev!)
-            targets: [
-                {
-                    src: './resources/js/plugins/curriculummedia/plugin.js',
-                    dest: './build/../../node_modules/tinymce/plugins/curriculummedia'
-                }
-            ]
         }),
         vue({
             template: {

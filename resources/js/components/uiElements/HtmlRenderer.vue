a1bb0d9e51f017d9e01ff32b2880ea4bb5a207b3
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-02-28 12:44:37 +0100

    HtmlRenderer: fixed not re-rendering when content got updated

--- resources/js/components/uiElements/HtmlRenderer.vue
+++ resources/js/components/uiElements/HtmlRenderer.vue
@@ -1,90 +1,85 @@
 <template>
-     <template
-        v-for="(part, index) in formattedParts"
-        :key="index">
-         <span
-             v-if="!part.component && typeof (part) !== 'undefined'"
-             v-html="part" v-inline>
-         </span>
-        <component
+    <template v-for="(part, index) in formattedParts"
+        :key="index"
+    >
+        <component v-if="part.component"
             :is="part.component"
-            v-bind="part.props">
-        </component>
+            v-bind="part.props"
+        ></component>
+        <span v-else
+            v-html="part"
+        ></span>
     </template>
 </template>
-
 <script>
 import LinkItem from "./LinkItem.vue";
 import Html from "./html.vue";
-    export default {
-        props: {
-            htmlContent: {
-                type: String,
-                required: true
-            }
-        },
-        data() {
-            return {
-                componentId: this.$.uid,
-            }
-        },
-        computed: {
-            formattedParts() {
-                let parts = [];
-                const regex = /(<a[^>]+href=["|'](.*?)["|'][^>]*>(.*)?<\/a>)?((?:.(?!<a))*.)?/gu;
 
-                let m;
-                let html;
-                while ((m = regex.exec(this.htmlContent)) !== null) {
-                    // This is necessary to avoid infinite loops with zero-width matches
-                    if (m.index === regex.lastIndex) {
-                        regex.lastIndex++;
-                    }
-                    let ma = [];
-                    m.forEach((match, groupIndex) => {
-                        ma.push(match);
-                    });
-                    let matches = [];
-                    matches.push(ma);
-
-                    //console.log(matches);
-                    matches.forEach((match) => {
-                        if(typeof (match[2]) !==  'undefined'){
-                            //console.log(match);
-                            if (this.isValidHttpUrl(match[2])) {
-                                parts.push({
-                                    component: 'LinkItem',
-                                    props: { href: match[2], text: match[3] }
-                                });
-                            }
-                        } else {
-                            parts.push( match[0]);
-                        }
-                    });
-                }
-                return parts;
-            }
-        },
-        mounted() {
-
-            console.log('mounted');
-        },
-        methods:{
-            isValidHttpUrl(string) {
-                let url;
-
-                try {
-                    url = new URL(string);
-                } catch (_) {
-                    return false;
-                }
-
-                return url.protocol === "http:" || url.protocol === "https:";
-            }
-        },
-        components: {
-            LinkItem,
-            Html
+export default {
+    props: {
+        htmlContent: {
+            type: String,
+            required: true,
         }
-    }
+    },
+    data() {
+        return {
+            componentId: this.$.uid,
+        }
+    },
+    computed: {
+        formattedParts() {
+            let parts = [];
+            const regex = /(<a[^>]+href=["|'](.*?)["|'][^>]*>(.*)?<\/a>)?((?:.(?!<a))*.)?/gu;
+
+            let m;
+            while ((m = regex.exec(this.htmlContent)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === regex.lastIndex) {
+                    regex.lastIndex++;
+                }
+                let ma = [];
+                m.forEach((match, groupIndex) => {
+                    ma.push(match);
+                });
+                let matches = [];
+                matches.push(ma);
+
+                //console.log(matches);
+                matches.forEach((match) => {
+                    if (typeof (match[2]) !==  'undefined') {
+                        //console.log(match);
+                        if (this.isValidHttpUrl(match[2])) {
+                            parts.push({
+                                component: 'LinkItem',
+                                props: { href: match[2], text: match[3] }
+                            });
+                        }
+                    } else {
+                        parts.push( match[0]);
+                    }
+                });
+            }
+            return parts;
+        }
+    },
+    mounted() {},
+    methods: {
+        isValidHttpUrl(string) {
+            let url;
+
+            try {
+                url = new URL(string);
+            } catch (_) {
+                return false;
+            }
+
+            return url.protocol === "http:" || url.protocol === "https:";
+        }
+    },
+    components: {
+        LinkItem,
+        Html,
+    },
+}
 </script>

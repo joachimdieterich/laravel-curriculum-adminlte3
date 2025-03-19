881c3a80e0055c4e866c87092f16ee635919e837
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-03-19 11:40:08 +0100

    simplified query

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -71,8 +71,8 @@ class UsersController extends Controller
         else
         {
             $users = (auth()->user()->role()->id == 1)
-                ? User::noSharing()->select('id', 'username', 'firstname', 'lastname', 'common_name', 'email', 'medium_id', 'deleted_at')
-                : Organization::where('id', auth()->user()->current_organization_id)->get()->first()->users()->noSharing();
+                ? User::select('id', 'username', 'firstname', 'lastname', 'common_name', 'email', 'medium_id', 'deleted_at')->noSharing()->get()
+                : Organization::find(auth()->user()->current_organization_id)->users()->noSharing()->get();
 
         }
 

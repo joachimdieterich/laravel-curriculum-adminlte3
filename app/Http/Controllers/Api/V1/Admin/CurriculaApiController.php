2dca742faf6b58e4abf0d73913331223d27becc2
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-04-09 10:37:00 +0200

    wip on #594

--- app/Http/Controllers/Api/V1/Admin/CurriculaApiController.php
+++ app/Http/Controllers/Api/V1/Admin/CurriculaApiController.php
@@ -11,9 +11,32 @@ use Illuminate\Http\Request;
 
 class CurriculaApiController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Curriculum::all();
+        $config = Config::where([
+            ['key', '=',  'metadata_password'],
+        ])->first();
+
+        if ($config !== null) {
+            if ($config->value != $request->query('password')) {
+                return response()->json(['error' => 'Not authorized.'], 403);
+            }
+        } else {
+            return response()->json(['error' => 'config (metadata_password) missing'], 420);
+        }
+
+        LogController::set(get_class($this).'@'.__FUNCTION__);
+        // might need to be broken down in chunks, since there's a lot of data
+        return Curriculum::select('id', 'title', 'description', 'grade_id', 'subject_id', 'organization_type_id', 'type_id')
+            ->where('type_id', 1)
+            ->with([
+                'grade',
+                'subject',
+                'organization_type',
+                'terminalObjectives',
+                'terminalObjectives.enablingObjectives'
+            ])
+            ->get();
     }
 
     public function show(Curriculum $curriculum)

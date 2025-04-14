a6a4814ba3111765cc0b10173e187129721256a1
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-04-14 10:59:56 +0200

    ObjectiveModals: added code-plugin for description-Editor

--- resources/js/components/objectives/EnablingObjectiveModal.vue
+++ resources/js/components/objectives/EnablingObjectiveModal.vue
@@ -188,7 +188,7 @@ export default {
             ),
             tinyMCE_description: this.$initTinyMCE(
                 [
-                    "autolink link table lists autoresize"
+                    "autolink link table lists autoresize code"
                 ],
                 {
                     public: 1,
@@ -197,7 +197,7 @@ export default {
                     subscribable_type: 'App\\Curriculum',
                     callbackId: this.component_id,
                 },
-                "bold underline italic | alignleft aligncenter alignright alignjustify | bullist numlist | link",
+                "bold underline italic | alignleft aligncenter alignright alignjustify | bullist numlist | link code",
                 ""
             ),
         }

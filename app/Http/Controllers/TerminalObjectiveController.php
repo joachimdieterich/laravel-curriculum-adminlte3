340e934c146eb11bd9692f7832c96240e37b371d
Alexander Lang <alexander.lang@pl.rlp.de>
2025-04-15 14:16:14 +0200

    TerminalObjectiveController: fixed update-logic

--- app/Http/Controllers/TerminalObjectiveController.php
+++ app/Http/Controllers/TerminalObjectiveController.php
@@ -80,18 +80,19 @@ class TerminalObjectiveController extends Controller
 
         // update objective type
         if ($request->has('objective_type_id')) {
-            $order_id = $old_objective->order_id;
-            if ($old_objective->objective_type_id != $request['objective_type_id']) {
-                $order_id = $this->getMaxOrderId(request('curriculum_id'), request('objective_type_id'));
-            }
-            $request->request->add(['order_id' => $order_id]);
             // if moved to another curriculum
             if ($old_objective->curriculum_id != request('curriculum_id')) {
+                $order_id = $this->getMaxOrderId(request('curriculum_id'), request('objective_type_id'));
                 if ($terminalObjective->update(['curriculum_id' => request('curriculum_id'), 'order_id' => $order_id]) == true) {
                     $this->moveToCurriculum($old_objective, $request);
                 }
-            } else if (($terminalObjective->update($request->all()) == true) and ($old_objective->order_id != request('order_id'))) {
-                $this->resetOrderIds($old_objective->curriculum_id, $old_objective->objective_type_id, $old_objective->order_id);
+            } else {
+                // if objective type got changed
+                if ($old_objective->objective_type_id != $request['objective_type_id']) {
+                    $order_id = $this->getMaxOrderId(request('curriculum_id'), request('objective_type_id'));
+                    $request->request->add(['order_id' => $order_id]);
+                }
+                $terminalObjective->update($request->all());
             }
             if (request()->wantsJson()) {
                 return $terminalObjective->without('curriculum')->with('type')->find($terminalObjective->id);

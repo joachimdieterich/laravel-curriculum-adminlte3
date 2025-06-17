1e1e3cad123279d21f76396be004633a90ce367f
Alexander Lang <alexander.lang@pl.rlp.de>
2025-06-17 09:50:02 +0200

    closed #394

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,16 +10,16 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 class Authenticate extends Middleware
 {
     public function handle($request, Closure $next, ...$guards) {
-        // rough sketch of SSO login handling
         // INFO: Token-Routes need to be changed to go through this middleware
         // only redirect to SSO login if request isn't a sharing-token link
-        // if (auth()->user() === null and !$request->has('sharing_token')) {
-        //     // loadOneLogin... always redirects to SSO login
-        //     $saml2Auth = new Saml2Auth(Saml2Auth::loadOneLoginAuthFromIpdConfig('rlp'));
-        //     return $saml2Auth->login(URL::full());
-        // }
+        if (auth()->user() === null and !$request->has('sharing_token')) {
+            // redirect to SSO login page
+            $saml2Auth = new Saml2Auth(Saml2Auth::loadOneLoginAuthFromIpdConfig('rlp'));
+            return $saml2Auth->login(URL::full());
+        }
 
-        Middleware::authenticate($request, $guards); // authenticate as guest if no user is logged in
+        // authenticate as guest if no user is logged in (only on sharing-token links)
+        Middleware::authenticate($request, $guards);
 
         return $next($request);
     }

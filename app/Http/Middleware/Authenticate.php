ba1dfaf5e976fa0844f97a593930370797de9eac
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-06-18 07:06:51 +0200

    ignore SSO in local environments

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,17 +5,23 @@ namespace App\Http\Middleware;
 use Closure;
 use Aacotroneo\Saml2\Saml2Auth;
 use Illuminate\Support\Facades\URL;
+use App\Http\Controllers\LogController;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
     public function handle($request, Closure $next, ...$guards) {
-        // INFO: Token-Routes need to be changed to go through this middleware
         // only redirect to SSO login if request isn't a sharing-token link
-        if (auth()->user() === null and !$request->has('sharing_token')) {
-            // redirect to SSO login page
-            $saml2Auth = new Saml2Auth(Saml2Auth::loadOneLoginAuthFromIpdConfig('rlp'));
-            return $saml2Auth->login(URL::full());
+        if (env('APP_ENV') != 'local' && auth()->user() === null) {
+            if ($request->has('sharing_token')) {
+                // set statistics for guest-authentication
+                LogController::set('guestLogin');
+                LogController::setStatistics();
+            } else {
+                // redirect to SSO login page
+                $saml2Auth = new Saml2Auth(Saml2Auth::loadOneLoginAuthFromIpdConfig('rlp'));
+                return $saml2Auth->login(URL::full());
+            }
         }
 
         // authenticate as guest if no user is logged in (only on sharing-token links)

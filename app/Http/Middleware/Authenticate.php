4a767db86de5e7cf3138768997db85274789419f
Alexander Lang <alexander.lang@pl.rlp.de>
2025-06-20 07:43:28 +0200

    #394 changed SSO-logic for sharing-token links

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,16 +11,22 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 class Authenticate extends Middleware
 {
     public function handle($request, Closure $next, ...$guards) {
-        // only redirect to SSO login if request isn't a sharing-token link
-        if (env('APP_ENV') != 'local' && auth()->user() === null) {
-            if ($request->has('sharing_token')) {
-                // set statistics for guest-authentication
-                LogController::set('guestLogin');
-                LogController::setStatistics();
+        if (auth()->user() === null && env('APP_ENV') != 'local') {
+            $saml2Auth = new Saml2Auth(Saml2Auth::loadOneLoginAuthFromIpdConfig('rlp'));
+            // check if user is already authenticated at IDP
+            if ($saml2Auth->isAuthenticated()) {
+                $common_name = $saml2Auth->getSaml2User()->getAttribute('cn')[0];
+                // authenticate user by common name
+                \Illuminate\Support\Facades\Auth::login(\App\User::where('common_name', $common_name)->firstOrFail());
             } else {
-                // redirect to SSO login page
-                $saml2Auth = new Saml2Auth(Saml2Auth::loadOneLoginAuthFromIpdConfig('rlp'));
-                return $saml2Auth->login(URL::full());
+                // only redirect to SSO login if request isn't a sharing-token link
+                if ($request->has('sharing_token')) {
+                    // set statistics for guest-authentication
+                    LogController::set('guestLogin');
+                    LogController::setStatistics();
+                } else {
+                    return $saml2Auth->login(URL::full()); // redirect to SSO login page
+                }
             }
         }
 

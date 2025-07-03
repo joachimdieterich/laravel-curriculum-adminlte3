2677add39621a2e0e18143184b5db4777478eebb
Alexander Lang <alexander.lang@pl.rlp.de>
2025-07-03 14:03:28 +0200

    show login-button for guest-user and trigger SSO on click

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,13 +12,13 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 class Authenticate extends Middleware
 {
     public function handle($request, Closure $next, ...$guards) {
-        if (auth()->user() === null && env('APP_ENV') != 'local') {
+        if ((auth()->user() === null or auth()->user()->id == env('GUEST_USER')) and env('APP_ENV') != 'local') {
             $saml2Auth = new Saml2Auth(Saml2Auth::loadOneLoginAuthFromIpdConfig('rlp'));
             // check if user is already authenticated at IDP
             if ($saml2Auth->isAuthenticated()) {
                 $common_name = $saml2Auth->getSaml2User()->getAttribute('cn')[0];
                 // authenticate user by common name
-                Auth::login(\App\User::where('common_name', $common_name)->firstOrFail());
+                Auth::login(\App\User::where('common_name', $common_name)->firstOrFail(), true);
             } else {
                 // only redirect to SSO login if request isn't available to guests
                 if (

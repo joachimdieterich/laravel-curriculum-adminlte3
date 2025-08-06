9d6a5de279e037cf567b69d110ea034cb988955c
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-08-06 07:23:39 +0200

    updated MapMarkerCategory/-Type-Seeder

--- database/seeders/MapMarkerCategorySeeder.php
+++ database/seeders/MapMarkerCategorySeeder.php
@@ -13,9 +13,12 @@ class MapMarkerCategorySeeder extends Seeder
     public function run(): void
     {
         MapMarkerCategory::insert([
-            ['title' => 'Bildunsmarker', 'color' => '#000000', 'owner_id' => 1],
-            ['title' => 'Pädagogisches Landesinstitut', 'color' => '#000000', 'owner_id' => 1],
-            ['title' => 'Digitales Kompetenzzentrum', 'color' => '#000000', 'owner_id' => 1],
+            ['title' => 'Bildunsmarker', 'color' => '#ABB2B9', 'owner_id' => 1],
+            ['title' => 'Fortbildungen', 'color' => '#85C1E9', 'owner_id' => 1],
+            ['title' => 'Pädagogisches Landesinstitut', 'color' => '#237d74', 'owner_id' => 1],
+            ['title' => 'Digitales Kompetenzzentrum', 'color' => '#342412', 'parent_id' => 3, 'owner_id' => 1],
+            ['title' => 'RegiKomp', 'color' => '#442342', 'parent_id' => 4, 'owner_id' => 1],
+            ['title' => 'KMZ', 'color' => '#1443f2', 'parent_id' => 5, 'owner_id' => 1],
         ]);
     }
 }

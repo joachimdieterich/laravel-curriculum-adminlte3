5852547633656b18a8a86d0adbde5fd1f51f12cc
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-08-15 10:50:38 +0200

    removed unused packge @activix/bootstrap-datetimepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 require('./bootstrap');
-require('@activix/bootstrap-datetimepicker');
 
 //vue
 import { createApp } from 'vue';
@@ -564,45 +563,5 @@ $(document).ready(function () {
 
     moment.updateLocale('en', {
         week: {dow: 1} // Monday is the first day of the week
-    });
-
-    $('.date').datetimepicker({
-        format: 'YYYY-MM-DD',
-        locale: 'en'
-    });
-
-    $('.datetime').datetimepicker({
-        format: 'YYYY-MM-DD HH:mm:ss',
-        locale: 'en',
-        sideBySide: true
-    });
-
-    $('.timepicker').datetimepicker({
-        format: 'HH:mm:ss'
-    });
-
-    $('.select-all').click(function () {
-        let $select2 = $(this).parent().siblings('.select2');
-        $select2.find('option').prop('selected', 'selected');
-        $select2.trigger('change');
-    });
-    $('.deselect-all').click(function () {
-        let $select2 = $(this).parent().siblings('.select2');
-        $select2.find('option').prop('selected', '');
-        $select2.trigger('change');
-    });
-
-    $('.select2').select2();
-
-    $('.treeview').each(function () {
-        var shouldExpand = false;
-        $(this).find('li').each(function () {
-            if ($(this).hasClass('active')) {
-                shouldExpand = true;
-            }
-        });
-        if (shouldExpand) {
-            $(this).addClass('active');
-        }
     });
 });

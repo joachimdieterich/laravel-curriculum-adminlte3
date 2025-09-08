bfef3d08929ecf87f9cf8a14f8d62f82d813b33c
Christian Repin <christian.repin@pl.rlp.de>
2025-09-08 09:02:28 +0200

    [#1088] don't start ws connection if env is false

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,30 +1,36 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
-window.Pusher = Pusher;
+let echo = {};
 
-const echoOptions = {
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['websocket', 'ws', 'wss'],
-    csrfToken: document.head.querySelector('meta[name="csrf-token"]'),
-    enableLogging: true, // Aktiviert Logging
-    auth: {
-        headers: {
-            'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]')
+const shouldConnectToWS = import.meta.env.VITE_WEBSOCKET_APP_ACTIVE == true;
+if (shouldConnectToWS === true) {
+    window.Pusher = Pusher;
+
+    const echoOptions = {
+        broadcaster: 'reverb',
+        key: import.meta.env.VITE_REVERB_APP_KEY,
+        wsHost: import.meta.env.VITE_REVERB_HOST,
+        wsPort: import.meta.env.VITE_REVERB_PORT,
+        wssPort: import.meta.env.VITE_REVERB_PORT,
+        forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+        enabledTransports: ['websocket', 'ws', 'wss'],
+        csrfToken: document.head.querySelector('meta[name="csrf-token"]'),
+        enableLogging: true, // Aktiviert Logging
+        auth: {
+            headers: {
+                'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]')
+            }
         }
-    }
-};
-const echo = new Echo(
-    {
-        ...echoOptions,
-        ...{
-            Pusher
+    };
+    echo = new Echo(
+        {
+            ...echoOptions,
+            ...{
+                Pusher
+            }
         }
-    }
-);
+    );
+}
+
 export default echo;

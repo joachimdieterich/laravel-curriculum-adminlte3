6783abf078b3010e41c3967fffd34be281a2bde8
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-09-30 14:25:15 +0200

    MediumPreviewModal: further improvements

--- resources/js/components/media/MediumPreviewModal.vue
+++ resources/js/components/media/MediumPreviewModal.vue
@@ -19,11 +19,11 @@
                     data-interval="false"
                     data-touch="true"
                 >
+                    <!-- slider arrows left/right -->
                     <div v-if="media.length > 1"
                         class="position-absolute d-flex align-items-center"
                         style="inset: 0"
                     >
-                        <!-- slider arrows left/right -->
                         <button
                             class="btn btn-icon-alt btn-icon-big highlight position-absolute"
                             style="left: 1rem; z-index: 1;"
@@ -38,7 +38,6 @@
                         >
                             <i class="fa fa-2x fa-angle-right"></i>
                         </button>
-                        <!-- preview gallery at bottom -->
                     </div>
                     <div class="carousel-inner d-flex align-items-center">
                         <div v-for="(medium, index) in media"
@@ -48,7 +47,7 @@
                         >
                             <i class="fa fa-spinner fa-pulse fa-3x fa-fw position-absolute text-white"></i>
                             <img
-                                :src="'/media/' + medium.id"
+                                :src="'/media/' + medium.id + '?preview=true'"
                                 :alt="medium.title ?? medium.name"
                                 class="position-relative user-select-none"
                                 style="max-width: 90svw; max-height: 90svh;"
@@ -56,6 +55,23 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            <!-- preview gallery at bottom -->
+            <div v-if="media.length > 1"
+                class="position-absolute d-flex"
+                style="bottom: 1rem; gap: 1rem; z-index: 1;"
+            >
+                <button v-for="(medium, index) in media"
+                    class="gallery-item d-flex align-items-center justify-content-center bg-white border-0"
+                    :class="{ 'active': index === currentSlide }"
+                    @click="slideTo(index)"
+                >
+                    <img
+                        :src="'/media/' + medium.id + '?preview=true'"
+                        :alt="medium.title ?? medium.name"
+                        style="max-height: 100px; max-width: 100px;"
+                    >
+                </button>
             </div>
         </div>
     </Transition>
@@ -75,14 +91,22 @@ export default {
         return {
             component_id: this.$.uid,
             media: [],
+            currentSlide: 0,
+            sliding: false,
         }
     },
     methods: {
         prev() {
+            if (this.sliding) return;
             $('#carousel-' + this.component_id).carousel('prev');
         },
         next() {
+            if (this.sliding) return;
             $('#carousel-' + this.component_id).carousel('next');
+        },
+        slideTo(index) {
+            if (this.sliding) return;
+            $('#carousel-' + this.component_id).carousel(index);
         },
     },
     mounted() {
@@ -98,6 +122,11 @@ export default {
                         // carousel needs to be initialized to activate swipe functionality
                         this.$nextTick(() => {
                             $('#carousel-' + this.component_id).carousel();
+                            $('#carousel-' + this.component_id).on('slide.bs.carousel', e => {
+                                this.currentSlide = e.to;
+                                this.sliding = true;
+                            });
+                            $('#carousel-' + this.component_id).on('slid.bs.carousel', e => { this.sliding = false; });
                         });
                     }
                 }
@@ -106,3 +135,16 @@ export default {
     },
 }
 </script>
+<style scoped>
+.gallery-item {
+    flex: 1;
+    width: 100px;
+    height: 100px;
+    border-radius: 0.5rem;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+    filter: brightness(0.6);
+    transition: filter 0.3s ease;
+
+    &.active { filter: brightness(1) }
+}
+</style>

25a672534af18a4b0b571c55c297d66a32ffbeb4
Alexander Lang <alexanderlang1.al+github@gmail.com>
2025-12-10 12:01:16 +0100

    typo

--- database/seeders/MapMarkerCategorySeeder.php
+++ database/seeders/MapMarkerCategorySeeder.php
@@ -13,7 +13,7 @@ class MapMarkerCategorySeeder extends Seeder
     public function run(): void
     {
         MapMarkerCategory::insert([
-            ['title' => 'Bildunsmarker', 'color' => '#ABB2B9', 'parent_id' => NULL, 'owner_id' => 1],
+            ['title' => 'Bildungsmarker', 'color' => '#ABB2B9', 'parent_id' => NULL, 'owner_id' => 1],
             ['title' => 'Fortbildungen', 'color' => '#85C1E9', 'parent_id' => NULL, 'owner_id' => 1],
             ['title' => 'Pädagogisches Landesinstitut', 'color' => '#237d74', 'parent_id' => NULL, 'owner_id' => 1],
             ['title' => 'Digitales Kompetenzzentrum', 'color' => '#342412', 'parent_id' => 3, 'owner_id' => 1],

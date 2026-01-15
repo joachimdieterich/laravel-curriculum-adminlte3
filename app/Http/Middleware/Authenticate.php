5b029cd3110ec4ed689bb646d19005c9413a0103
Alexander Lang <alexander.lang@pl.rlp.de>
2026-01-15 08:50:55 +0100

    further improvements for OIDC-login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -21,54 +21,30 @@ class Authenticate extends Middleware
                 env('OIDC_CLIENT_SECRET')
             );
 
+            session_start();
             // store current URL to redirect back after login
             if (!isset($_SESSION['redirect_to'])) $_SESSION['redirect_to'] = URL::full();
             // $oidc->setCodeChallengeMethod('S256'); // PKCE
             $oidc->authenticate();
 
             try {
-                $userinfo = $oidc->requestUserInfo();
-                // dd($userinfo);
+                $common_name = $oidc->requestUserInfo('sub');
                 // login user by common_name
-                // Auth::login(\App\User::select('id')->where('common_name', $userinfo->common_name)->firstOrFail(), true);
+                Auth::login(\App\User::select('id')->where('common_name', $common_name)->firstOrFail(), true);
             } catch (\Throwable $th) {
                 // if user not authenticated, login as guest user
+                // if ($page_allows_guest) ...loginAsGuest
+                // else return redirect($to_idp_login_page);
                 // Auth::loginUsingId((env('GUEST_USER')), true);
                 throw $th; // for debugging
             }
 
             // since the user got redirected back after authentication, redirect to the originally requested URL
-            // if (isset($_REQUEST['code'])) {
-            //     $redirect = $_SESSION['redirect_to'];
-            //     unset($_SESSION['redirect_to']);
-            //     return redirect($redirect);
-            // }
-
-            /*** old SAML2-Auth ***/
-            // check if user is already authenticated at IDP
-            // if ($saml2Auth->isAuthenticated()) {
-            //     $common_name = $saml2Auth->getSaml2User()->getAttribute('cn')[0];
-            //     // authenticate user by common name
-            //     Auth::login(\App\User::where('common_name', $common_name)->firstOrFail(), true);
-            // } else {
-            //     // only redirect to SSO login if request isn't available to guests
-            //     if (
-            //         !$request->has('forceSSO') and // TODO: temporary solution | remove after implementing OIDC
-            //         (
-            //             $request->has('sharing_token')
-            //             or str_starts_with($request->getRequestUri(), '/navigator')
-            //             or str_starts_with($request->getRequestUri(), '/eventSubscriptions')
-            //             or str_ends_with($request->getPathInfo(), 'startWithPw') // videoconference-link
-            //         )
-            //     ) {
-            //         // set statistics for guest-authentication
-            //         LogController::set('guestLogin');
-            //         LogController::setStatistics();
-            //         Auth::loginUsingId((env('GUEST_USER')), true);
-            //     } else {
-            //         return $saml2Auth->login(URL::full()); // after successful login, redirect to the current URL
-            //     }
-            // }
+            if (isset($_REQUEST['code'])) {
+                $redirect = $_SESSION['redirect_to'];
+                unset($_SESSION['redirect_to']);
+                return redirect($redirect);
+            }
         }
         // needed to redirect to login-page in local environment
         Middleware::authenticate($request, $guards);

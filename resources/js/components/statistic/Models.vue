df68686032ffe4bb4c1c1ffa0758f76f8c279eda
Alexander Lang <alexander.lang@pl.rlp.de>
2026-01-29 12:19:43 +0100

    AdminPanel: fixed login-chart showing wrong data | formatting

--- resources/js/components/statistic/Models.vue
+++ resources/js/components/statistic/Models.vue
@@ -1,24 +1,27 @@
-<template :id="this.id "
-          class="p-2">
+<template>
     <div class="card">
         <div class="w-full flex-1 p-2">
-            {{ this.title }}
-            <input type="search"
-                   class="form-control form-control-sm"
-                   style="border:0;"
-                   :placeholder="trans('global.search')+'...'"
-                   v-model="search">
+            {{ title }}
+            <input
+                type="search"
+                class="form-control form-control-sm"
+                style="border:0;"
+                :placeholder="trans('global.search') + '...'"
+                v-model="search"
+            >
         </div>
-        <div class="card-footer bg-light p-0"
-             style="max-height:225px; overflow-y: auto">
+        <div
+            class="card-footer bg-light p-0"
+            style="max-height: 225px; overflow-y: auto"
+        >
             <ul class="nav nav-pills flex-column">
                 <li v-for="item in model_data"
                     class="nav-item"
-                    :style="isVisible(item)">
-                    <span  class="nav-link">
-                        {{item.value}}
-                        <span class="float-right">
-                        {{item.counter}}</span>
+                    :style="isVisible(item)"
+                >
+                    <span class="nav-link">
+                        {{ item.value }}
+                        <span class="float-right">{{item.counter}}</span>
                     </span>
                 </li>
             </ul>
@@ -26,34 +29,46 @@
     </div>
 </template>
 <script>
-
 export default {
     name: 'Models',
-    components: {
-    },
     props: {
-        'id' : String,
-        'title': String,
-        'chart': String,
-        'date_begin': String,
-        'date_end': String,
+        id: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        chart: {
+            type: String,
+            required: true,
+        },
+        date_begin: {
+            type: String,
+            required: true,
+        },
+        date_end: {
+            type: String,
+            required: true,
+        },
         width: {
             type: Number,
-            default: 200
+            default: 200,
         },
         height: {
             type: Number,
-            default: 400
+            default: 400,
         },
         cssClasses: {
+            type: String,
             default: '',
-            type: String
         },
         styles: {
             type: Object,
+            default: () => {},
         },
     },
-
     data() {
         return {
             search: '',
@@ -63,7 +78,7 @@ export default {
     methods: {
         loaderEvent() {
             this.model_data = [];
-            this.model = [
+            const models = [
                 'Certificate',
                 'Content',
                 'Curriculum',
@@ -100,50 +115,39 @@ export default {
                 'VideoconferenceSubscription',
             ];
 
-
-            this.model.forEach(
-                (model_name) => {
-                    axios.get('/statistics?chart=' + this.chart + '&model=' + model_name + '&date_begin=' + this.date_begin + '&date_end=' + this.date_end)
-                        .then(response => {
-                            this.model_data.push(response.data.message);
-                        }).catch(e => {
-                        console.log(e);
-                    });
-                }
-            );
+            models.forEach((model_name) => {
+                axios.get('/statistics?chart=' + this.chart + '&model=' + model_name + '&date_begin=' + this.date_begin + '&date_end=' + this.date_end)
+                    .then(response => {
+                        this.model_data.push(response.data);
+                    }).catch(e => console.log(e));
+            });
         },
-
-        isVisible(item){
-            if (item.value === null){
-                if (this.search.toLowerCase() !== ''){
-                    return "display:none";
-                } else {
-                    return "";
-                }
-            }
-            if (item.value.toLowerCase().indexOf(this.search.toLowerCase()) === -1){
-                return "display:none";
+        isVisible(item) {
+            if (item.value === null) {
+                return this.search.toLowerCase() !== ''
+                    ? "display: none"
+                    : "";
             } else {
-                return "";
+                return item.value.toLowerCase().indexOf(this.search.toLowerCase()) === -1
+                    ? "display: none"
+                    : "";
             }
         },
     },
     watch: {
         date_begin: {
-            handler: function(){
+            handler: function() {
                 this.loaderEvent();
-            }
+            },
         },
         date_end: {
-            handler: function(){
+            handler: function() {
                 this.loaderEvent();
-            }
-        }
-    },
-    computed: {
+            },
+        },
     },
     mounted() {
         this.loaderEvent();
-    }
+    },
 }
 </script>

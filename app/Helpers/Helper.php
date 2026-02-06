e846672f0b0a4c449f3782cf7f9dbad11449c66c
Christian Repin <christian.repin@pl.rlp.de>
2026-02-06 11:34:05 +0100

    [#1436] fix getEntriesForSelect2ByCollectionAlternative

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -156,55 +156,47 @@ if (! function_exists('getEntriesForSelect2ByCollectionAlternative'))
             'selected' => 'sometimes|nullable',
         ]);
 
-        if (request()->has('selected'))
-        {
-            //dump($collection->whereIn($table . $id, (array)$input['selected'])->get());
+        if (request()->has('selected')) {
             return response()->json($collection->whereIn($table . $id, (array)$input['selected'])->values());
         }
-        else
-        {
-            $page = $input['page'];
-            $resultCount = 25;
 
-            $offset = ($page - 1) * $resultCount;
+        $page        = $input['page'];
+        $resultCount = 25;
 
-            $term = strtolower($input['term']); // str_contains is case sensitive
+        $offset = ($page - 1) * $resultCount;
 
-            $allEntries = $collection->filter(function($obj) use ($field, $term) {
-                foreach ((array)$field as $f) {
-                    // if any match is true, return the entry
-                    if (str_contains(strtolower($obj[$f]), $term)) return true;
-                }
-                return false;
-            });
+        $term = strtolower($input['term']); // str_contains is case sensitive
 
-            $count = Count($allEntries);
+        $allEntries = $collection->filter(function($obj) use ($field, $term) {
+            return array_any((array) $field, fn($f) => str_contains(strtolower(((array) $obj)[$f]), $term));
+        });
 
-            $entries = $allEntries
-                ->sortBy($orderby, SORT_NATURAL)
-                ->skip($offset)
-                ->take($resultCount)
-                ->select([$table.$id, $text])
-                ->map(function($entry) use ($table, $id, $text) {
-                    return [
-                        'id' => $entry[$table.$id],
-                        'text' => $entry[$text],
-                    ];
-                })
-                ->values();
+        $count = Count($allEntries);
 
-            $endCount = $offset + $resultCount;
-            $morePages = $count > $endCount;
+        $entries = $allEntries
+            ->sortBy($orderby, SORT_NATURAL)
+            ->skip($offset)
+            ->take($resultCount)
+            ->select([$table.$id, $text])
+            ->map(function($entry) use ($table, $id, $text) {
+                return [
+                    'id' => $entry[$table.$id],
+                    'text' => $entry[$text],
+                ];
+            })
+            ->values();
 
-            $results = array(
-                "results" => $entries,
-                "pagination" => array(
-                    "more" => $morePages
-                )
-            );
+        $endCount  = $offset + $resultCount;
+        $morePages = $count > $endCount;
 
-            return response()->json($results);
-        }
+        $results = array(
+            "results" => $entries,
+            "pagination" => array(
+                "more" => $morePages
+            )
+        );
+
+        return response()->json($results);
     }
 }
 

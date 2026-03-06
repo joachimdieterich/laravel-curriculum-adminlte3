1242cb52453331e3ed13a26e91e978de533bfbf8
Alexander Lang <alexanderlang1.al+github@gmail.com>
2026-03-06 08:46:11 +0100

    OIDC: fixed guest-login logic

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,32 +10,36 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 class Authenticate extends Middleware
 {
     public function handle($request, Closure $next, ...$guards) {
-        $user = auth()->user();
+        $user_id = auth()->user()?->id;
 
-        if (($user === null or $user->id == env('GUEST_USER')) and env('APP_ENV') != 'local') {
-            $oidc = new OpenIDConnectClient(
-                env('OIDC_RLP_IDP_HOST'),
-                env('OIDC_CLIENT_ID'),
-                env('OIDC_CLIENT_SECRET')
-            );
-
-            // store current URL to redirect back after authentication-callback
-            if (!session('redirect_to')) {
-                session(['redirect_to' => URL::full()]);
-                \Session::save();
-            }
-            // $oidc->setCodeChallengeMethod('S256'); // PKCE
-
+        if (($user_id === null or $user_id == env('GUEST_USER')) and env('APP_ENV') != 'local') {
             $allow_guest = $request->has('sharing_token')
                 or str_starts_with($request->getRequestUri(), '/navigator')
                 or str_starts_with($request->getRequestUri(), '/eventSubscriptions')
                 or str_ends_with($request->getPathInfo(), 'startWithPw'); // videoconference-link;
-            // if resource is accessible for guests, request silent authentication
-            if ($allow_guest) $oidc->addAuthParam(['prompt' => 'none']);
 
-            // this will call the authorization endpoint and redirect to our OIDC-handling route
-            $oidc->setRedirectURL(env('APP_URL') . '/oidc');
-            $oidc->authenticate();
+            // skip authentication if authenticated as guest and guest access is allowed
+            if ($user_id != env('GUEST_USER') or !$allow_guest) {
+                $oidc = new OpenIDConnectClient(
+                    env('OIDC_RLP_IDP_HOST'),
+                    env('OIDC_CLIENT_ID'),
+                    env('OIDC_CLIENT_SECRET')
+                );
+    
+                // store current URL to redirect back after authentication-callback
+                if (!session('redirect_to')) {
+                    session(['redirect_to' => URL::full()]);
+                    \Session::save();
+                }
+                // $oidc->setCodeChallengeMethod('S256'); // PKCE
+                
+                // if resource is accessible for guests, request silent authentication
+                if ($allow_guest) $oidc->addAuthParam(['prompt' => 'none']);
+    
+                // this will call the authorization endpoint and redirect to our OIDC-handling route
+                $oidc->setRedirectURL(env('APP_URL') . '/oidc');
+                $oidc->authenticate();
+            }
         }
         // needed to redirect to login-page in local environment
         Middleware::authenticate($request, $guards);

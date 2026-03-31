69118cd493020e6330d15d3262884ab94b802b81
Alexander Lang <alexander.lang@pl.rlp.de>
2026-03-31 12:42:12 +0200

    revert commit

--- config/database.php
+++ config/database.php
@@ -132,12 +132,5 @@ return [
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 1),
         ],
-
-        'session' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_SESSION_DB', 0),
-        ],
     ],
 ];

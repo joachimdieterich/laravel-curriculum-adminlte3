6776814e84e750b3a299c0e724227aca63a15753
alexanderlang1al <alexanderlang1.al+github@gmail.com>
2026-05-20 11:37:12 +0200

    cached translations

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -15,15 +15,18 @@
 
     <script>
         window.trans = <?php
-        // copy all translations from /resources/lang/CURRENT_LOCALE/* to global JS variable
-        $lang_files = File::files(resource_path() . '/lang/' . App::getLocale());
-        $trans = [];
-        foreach ($lang_files as $f) {
-            $filename = pathinfo($f)['filename'];
-            $trans[$filename] = trans($filename);
-        }
-        echo json_encode($trans);
+            $translations = Cache::rememberForever('translations.' . App::getLocale(), function () {
+                $lang_files = File::files(resource_path() . '/lang/' . App::getLocale());
+                $trans = [];
+                foreach ($lang_files as $f) {
+                    $filename = pathinfo($f)['filename'];
+                    $trans[$filename] = trans($filename);
+                }
+                return json_encode($trans);
+            });
+            echo $translations;
         ?>;
+
         window.Laravel = <?php
         echo json_encode([
             'csrfToken' => csrf_token(),

7f088359c8ba38c28e525af1afb61a3ca204a4a0
Christian Repin <christian.repin@pl.rlp.de>
2026-06-18 11:18:00 +0200

    #1531 infinite ajax load in c-select / moved label frontend out of component / reduced level controller

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Level;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class LevelController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse|void
      */
     public function index()
     {
@@ -20,71 +19,5 @@ class LevelController extends Controller
             );
         }
         //return Level::all()->toJson();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Level  $level
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Level $level)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Level  $level
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Level $level)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Level  $level
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Level $level)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Level  $level
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Level $level)
-    {
-        //
     }
 }

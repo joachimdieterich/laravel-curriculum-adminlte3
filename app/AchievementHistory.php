ba7c90a8765af807ae855b00b6a3bb67c3e335f1
alexanderlang1al <alexanderlang1.al+github@gmail.com>
2026-06-22 09:28:09 +0200

    AchievementHistory: added owner-relation

--- app/AchievementHistory.php
+++ app/AchievementHistory.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class AchievementHistory extends Model
 {
@@ -24,5 +25,10 @@ class AchievementHistory extends Model
     public function achievement(): BelongsTo
     {
         return $this->belongsTo(Achievement::class);
+    }
+
+    public function owner(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'owner_id')->select('id', 'firstname', 'lastname');
     }
 }

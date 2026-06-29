0074f093e6fd47ea72614c81aa92813e3cd2786e
alexanderlang1al <alexanderlang1.al+github@gmail.com>
2026-06-29 10:17:52 +0200

    removed sharing_token-column in users-table

--- database/migrations/2026_06_29_101044_delete_users_sharing_token.php
+++ database/migrations/2026_06_29_101044_delete_users_sharing_token.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->index('sharing_token');
+            $table->dropColumn('sharing_token');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropIndex('sharing_token');
+            $table->string('sharing_token')->nullable();
         });
     }
 };

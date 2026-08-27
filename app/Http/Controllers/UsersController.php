a29e6d3c142f99b8309479abc606192b6f896222
Alexander Lang <alexander.lang@pl.rlp.de>
2026-08-27 10:55:36 +0200

    fixed listing users in /groups/{id} for teacher-role

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -71,7 +71,8 @@ class UsersController extends Controller
 
     public function list()
     {
-        abort_unless(\Gate::allows('user_access'), 403);
+        // don't use 'user_access' since this would cause the teacher-role to have access to /users
+        abort_unless(\Gate::allows('user_show'), 403);
 
         $rowID = 'id';
 

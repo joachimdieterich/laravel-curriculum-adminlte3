81be1f78d5f470d834bf4232b28258f91a101888
alexanderlang1al <alexanderlang1.al+github@gmail.com>
2026-08-28 13:14:48 +0200

    CurriculumController: fixed not fetching globally available Curricula

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -249,9 +249,10 @@ if (! function_exists('getDataTableWithEntries'))
      * helper function to get all entries for select2 fields
      * @param Illuminate\Database\Eloquent\Builder $query the model query to use, e.g. Kanban::select() or $user->kanbans() (without get()!)
      * @param bool $hasTags does the model have tags?
+     * @param bool $global include ressources that are globally available (e.g. for Curricula)
      * @return \Illuminate\Http\JsonResponse
      */
-    function getDataTableWithEntries($query, $hasTags = false): \Illuminate\Http\JsonResponse
+    function getDataTableWithEntries($query, $hasTags = false, $global = false): \Illuminate\Http\JsonResponse
     {
         $withOwned = false;
         $withSubscribed = false;
@@ -301,6 +302,8 @@ if (! function_exists('getDataTableWithEntries'))
         try {
             if ($withSubscribed) $query = getSubscribedModels($query, $withOwned);
             else if ($withOwned) $query->orWhere('owner_id', auth()->user()->id); // only get owned entries
+
+            if ($global && request('filter') === 'all') $query->orWhere('type_id', 1);
 
             // only apply tag-filters if model has tags
             if ($hasTags) {

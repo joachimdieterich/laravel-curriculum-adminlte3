aeee2e24dcbc57eb0d240f5c25e218a72458e0b0
alexanderlang1al <alexanderlang1.al+github@gmail.com>
2026-09-09 12:39:18 +0200

    formatting

--- resources/views/kanbans/index.blade.php
+++ resources/views/kanbans/index.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.master')
 @section('content')
-    <kanbans model-url="kanbans"></kanbans>
+    <Kanbans></Kanbans>
 @endsection
--- resources/views/kanbans/show.blade.php
+++ resources/views/kanbans/show.blade.php
@@ -7,12 +7,11 @@
 @endsection
 @section('content')
     <div class="h-100">
-        <kanban
+        <Kanban
             :editable="{{ $may_edit ? 'true' : 'false' }}"
             :favourable="{{ $may_favour ? 'true' : 'false' }}"
             :websocket="{{ $is_websocket_active ? 'true' : 'false' }}"
-            ref="kanbanBoard"
             :initial-kanban="{{ $kanban }}"
-        ></kanban>
+        ></Kanban>
     </div>
 @endsection

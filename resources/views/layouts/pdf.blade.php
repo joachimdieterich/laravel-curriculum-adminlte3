8f176285bda1f095608995caf7c58f0d26e24d62
Christian Repin <christian.repin@pl.rlp.de>
2026-09-10 11:33:45 +0200

    yield the content correct for correct encoding

--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ App::getLocale() }}">
     <head>
+        <meta charset="UTF-8">
         @vite(['resources/sass/app.scss'])
     </head>
     <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">

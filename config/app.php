e3f4c8ebc631190368cb6616476f0b26915aabd9
alexanderlang1al <alexanderlang1.al+github@gmail.com>
2026-09-11 08:53:59 +0200

    fixed alias

--- config/app.php
+++ config/app.php
@@ -242,7 +242,7 @@ return [
         'File'         => Illuminate\Support\Facades\File::class,
         'Gate'         => Illuminate\Support\Facades\Gate::class,
         'Hash'         => Illuminate\Support\Facades\Hash::class,
-        'Image'        => Intervention\Image\Facades\Image::class,
+        'Image'        => Intervention\Image\Laravel\Facades\Image::class,
         'Lang'         => Illuminate\Support\Facades\Lang::class,
         'Log'          => Illuminate\Support\Facades\Log::class,
         'Mail'         => Illuminate\Support\Facades\Mail::class,

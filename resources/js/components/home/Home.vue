588571e0133b74a8404f35d0d3fdb2f3d6acaf91
Christian Repin <christian.repin@pl.rlp.de>
2026-09-16 16:44:06 +0200

    css bugfix / curricula in home always visible / added curriculum modal to home

--- resources/js/components/home/Home.vue
+++ resources/js/components/home/Home.vue
@@ -12,15 +12,16 @@
                 :text="trans('global.curriculum.title')"
                 icon="fa-th"
                 icon-background-class="bg-cyan"
-                :hide-if-empty="true"
                 href="/curricula"
+                :has-modal="true"
+                @open-modal="openModal('curriculum-modal')"
                 @error="handleError"
             >
                 <template #entry="{ entry }">
                     <a :href="'/courses/' + entry.course_id">
                         <span class="font-weight-bold">{{ entry.title }}</span>
                         <span v-if="isVisible.progress"
-                            class="pull-right w-50"
+                              class="pull-right w-50"
                         >
                             <ProgressBar
                                 :achievements="entry.achievements"
@@ -34,21 +35,21 @@
             </InfoBox>
 
             <InfoBox v-if="isVisible.groups"
-                model="groups"
-                :text="trans('global.group.title')"
-                icon="fa-users"
-                icon-background-class="bg-purple"
-                @error="handleError"
+                     model="groups"
+                     :text="trans('global.group.title')"
+                     icon="fa-users"
+                     icon-background-class="bg-purple"
+                     @error="handleError"
             />
 
             <InfoBox v-if="isVisible.achievements"
-                model="achievements"
-                :disable-link="true"
-                :text="trans('global.achievement.recent')"
-                icon="fa-trophy"
-                icon-background-class="bg-blue"
-                :hide-if-empty="true"
-                @error="handleError"
+                     model="achievements"
+                     :disable-link="true"
+                     :text="trans('global.achievement.recent')"
+                     icon="fa-trophy"
+                     icon-background-class="bg-blue"
+                     :hide-if-empty="true"
+                     @error="handleError"
             >
                 <template #entry="{ entry }">
                     <span
@@ -57,7 +58,7 @@
                         style="gap: 0.5rem;"
                     >
                         <span v-if="entry.history.length > 0"
-                            class="d-flex align-items-center"
+                              class="d-flex align-items-center"
                         >
                             <i
                                 class="text-green t-20 mr-1"
@@ -104,7 +105,7 @@
                 model="logbooks"
                 :text="trans('global.logbook.title')"
                 icon="fa-book"
-                icon-background-class="bg-red"    
+                icon-background-class="bg-red"
                 :has-modal="true"
                 @open-modal="openModal('logbook-modal')"
                 @error="handleError"
@@ -119,7 +120,7 @@
                 @open-modal="openModal('kanban-modal')"
                 @error="handleError"
             />
-    
+
             <InfoBox
                 model="plans"
                 :text="trans('global.plan.title')"
@@ -132,11 +133,11 @@
             />
 
             <InfoBox v-if="isVisible.users"
-                model="users"
-                :text="trans('global.user_management')"
-                icon="fa-user"
-                icon-background-class="bg-blue"
-                :header-only="true"
+                     model="users"
+                     :text="trans('global.user_management')"
+                     icon="fa-user"
+                     icon-background-class="bg-blue"
+                     :header-only="true"
             />
 
             <InfoBox
@@ -162,6 +163,7 @@
                 </template>
             </InfoBox>
         </div>
+        <CurriculumModal/>
         <LogbookModal/>
         <KanbanModal/>
         <PlanModal/>
@@ -170,6 +172,7 @@
     </div>
 </template>
 <script>
+import '@vuepic/vue-datepicker/dist/main.css';
 import InfoBox from '../uiElements/InfoBox.vue';
 import ProgressBar from '../uiElements/ProgressBar.vue';
 import LogbookModal from '../logbook/LogbookModal.vue';
@@ -177,8 +180,9 @@ import KanbanModal from '../kanban/KanbanModal.vue';
 import PlanModal from '../plan/PlanModal.vue';
 import SubscribeExamModal from '../exam/SubscribeExamModal.vue';
 import MediumModal from '../media/MediumModal.vue';
-import { useGlobalStore } from '../../store/global';
-import { useToast } from 'vue-toastification';
+import {useGlobalStore} from '../../store/global';
+import {useToast} from 'vue-toastification';
+import CurriculumModal from "../curriculum/CurriculumModal.vue";
 
 export default {
     name: 'Home',
@@ -191,6 +195,9 @@ export default {
         };
     },
     mounted() {
+        this.$eventHub.on('curriculum-added', (curriculum) => {
+            window.location.href = '/curricula/' + curriculum.id;
+        });
         this.$eventHub.on('logbook-added', (logbook) => {
             window.location.href = '/logbooks/' + logbook.id;
         });
@@ -241,6 +248,7 @@ export default {
         },
     },
     components: {
+        CurriculumModal,
         InfoBox,
         ProgressBar,
         LogbookModal,

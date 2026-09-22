1f56881b171176c4f0f15d50ca259176c53b8b7e
Christian Repin <christian.repin@pl.rlp.de>
2026-09-22 11:31:36 +0200

    2253: db migrations

---
+++ database/migrations/2026_09_22_103554_change_group_user_foreign_key.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('group_user', function (Blueprint $table) {
+            $table->dropForeign(['group_id']);
+            $table->foreign('group_id')
+                ->references('id')->on('groups')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['group_id']);
+            $table->foreign('group_id')->references('id')->on('groups');
+        });
+    }
+};
